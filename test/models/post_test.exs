040ca02f3042f44fb030caf939159d9f7bcc82dd
Tony Daly <tony.daly@me.com>
2017-01-23 21:23:32 +0000

    Model tests.

---
+++ test/models/post_test.exs
@@ -0,0 +1,17 @@
+defmodule Healthlocker.PostTest do
+  use Healthlocker.ModelCase, async: true
+  alias Healthlocker.Post
+
+  @valid_attrs %{content: "Hello, world!"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Post.changeset(%Post{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Post.changeset(%Post{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
