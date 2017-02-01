7adbc2e9d60758d8e649f271c59d568ac1762735
katbow <katherine.bowler@gmail.com>
2017-02-01 14:57:11 +0000

    Remove unnecessary files for tips and modify index to display tips from posts table #7 #36

--- web/controllers/tip_controller.ex
+++ web/controllers/tip_controller.ex
@@ -1,33 +1,15 @@
 defmodule Healthlocker.TipController do
   use Healthlocker.Web, :controller
 
-  alias Healthlocker.Tip
+  alias Healthlocker.Post
 
   def index(conn, params) do
     tips = if params["tag"] do
-            Repo.all(from t in Tip, where: t.tag == ^params["tag"])
+            Repo.all(from p in Post, where: ilike(p.content, ^"%##{params["tag"]}%"))
           else
-            Repo.all(Tip)
+            Repo.all(from p in Post, where: ilike(p.content, "%#tip%"))
           end
-    render(conn, "index.html", tips: tips)
-  end
-
-  def new(conn, _params) do
-    changeset = Tip.changeset(%Tip{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"tip" => tip_params}) do
-    changeset = Tip.changeset(%Tip{}, tip_params)
-
-    case Repo.insert(changeset) do
-      {:ok, _tip} ->
-        conn
-        |> put_flash(:info, "Tip created successfully.")
-        |> redirect(to: tip_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
+    render(conn, "index.html", posts: tips)
   end
 
 end
