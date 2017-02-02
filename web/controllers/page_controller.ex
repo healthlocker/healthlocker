5224580423da1679ef65172faf8a2b0e1a520fa7
katbow <katherine.bowler@gmail.com>
2017-02-02 17:27:47 +0000

    Add layout for homepage & db calls for single story and tip

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,15 @@
 defmodule Healthlocker.PageController do
   use Healthlocker.Web, :controller
 
+  alias Healthlocker.Post
+
   def index(conn, _params) do
-    render conn, "index.html"
+    featured_story = Repo.one from p in Post,
+                              limit: 1,
+                              where: ilike(p.content, "%#story%")
+    featured_tip = Repo.one from p in Post,
+                              limit: 1,
+                              where: ilike(p.content, "%#tip%")
+    render conn, "index.html", story: featured_story, tip: featured_tip
   end
 end
