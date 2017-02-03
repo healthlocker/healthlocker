1435fe2ba54f9f88c0666a5715ea8f30ef3916a7
katbow <katherine.bowler@gmail.com>
2017-02-03 15:45:44 +0000

    Add functionaliy for authorising users and page views #26
    Can be used for adding user cookie to session when logging in or signing up, and checking if cookie is present

---
+++ web/controllers/auth.ex
@@ -0,0 +1,36 @@
+defmodule Healthlocker.Auth do
+  import Plug.Conn
+  import Comeonin.Bcrypt, only: [checkpw: 2, dummy_checkpw: 0]
+
+  def init(opts) do
+    Keyword.fetch!(opts, :repo)
+  end
+
+  def call(conn, repo) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && repo.get(Healthlocker.User, user_id)
+    assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
+  def email_and_pass_login(conn, email, given_pass, opts) do
+    repo = Keyword.fetch!(opts, :repo)
+    user = repo.get_by(Healthlocker.User, email: email)
+
+    cond do
+      user && checkpw(given_pass, user.password_hash) ->
+        {:ok, login(conn, user)}
+      user ->
+        {:error, :unauthorized, conn}
+      true ->
+        dummy_checkpw()
+        {:error, :not_found, conn}
+    end
+  end
+end
