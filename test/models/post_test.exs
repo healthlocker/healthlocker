e9599ada61541695750781b05b36e2cc38754bb0
katbow <katherine.bowler@gmail.com>
2017-02-06 11:30:26 +0000

    Add tests for new model queries #63 #10

--- test/models/post_test.exs
+++ test/models/post_test.exs
@@ -1,6 +1,7 @@
 defmodule Healthlocker.PostTest do
   use Healthlocker.ModelCase, async: true
   alias Healthlocker.Post
+  import Healthlocker.Fixtures
 
   @valid_attrs %{content: "Hello world!"}
   @invalid_attrs %{}
@@ -13,5 +14,29 @@ defmodule Healthlocker.PostTest do
   test "changeset with invalid attributes" do
     changeset = Post.changeset(%Post{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "featured_story returns a story" do
+    post  = fixture(:post)
+    story = Post |> Post.featured_story |> Repo.one
+    assert String.contains? story.content, "#story"
+  end
+
+  test "featured_story does not return any tips" do
+    post  = fixture(:post)
+    story = Post |> Post.featured_story |> Repo.one
+    refute String.contains? story.content, "#tip"
+  end
+
+  test "featured_tip returns a tip" do
+    post  = fixture(:post)
+    tip = Post |> Post.featured_tip |> Repo.one
+    assert String.contains? tip.content, "#tip"
+  end
+
+  test "featured_tip does not return any stories" do
+    post  = fixture(:post)
+    tip = Post |> Post.featured_tip |> Repo.one
+    refute String.contains? tip.content, "#story"
   end
 end
