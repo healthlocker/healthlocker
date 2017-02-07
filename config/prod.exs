bb8f5070e9685abc3f0e8cac91fa63bc31b7321c
Tony Daly <tony.daly@me.com>
2017-02-07 12:28:33 +0000

    Fetch secret key base from an environment variable
    As Heroku use environment variables for everything we need to move the
    secret key base out of the `prod.secret.exs` file.

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :healthlocker, Healthlocker.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
