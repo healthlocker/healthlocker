d7d866b3f5b74e4d1f79656876e6de6405386ae2
katbow <katherine.bowler@gmail.com>
2017-02-07 12:41:07 +0000

    Change auth for call from test setup as doesn't work in production #78

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -8,14 +8,8 @@ defmodule Healthlocker.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    cond do
-      user = conn.assigns[:current_user] ->
-        conn
-      user = user_id && repo.get(Healthlocker.User, user_id)
-        assign(conn, :current_user, user)
-      true ->
-        assign(conn, :current_user, nil)
-    end
+    user = user_id && repo.get(Healthlocker.User, user_id)
+    assign(conn, :current_user, user)
   end
 
   def login(conn, user) do
