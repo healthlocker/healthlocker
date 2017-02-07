2278ad73c831038057f34daec5f663fce57546f6
Tony Daly <tony.daly@me.com>
2017-02-07 13:04:35 +0000

    Production host settings
    Get the host url from an environment variable and force ssl.

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :healthlocker, Healthlocker.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: System.get_env("HEROKU_URL") , port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
