a62df8fb0a6520778b2296429ec9a800ba2d6f42
katbow <katherine.bowler@gmail.com>
2017-02-09 14:38:05 +0000

    Update changesets for multiple forms #12
    First create needs to go to second part of the form & new actions are needed for rendering and using part2 of the form

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -13,7 +13,23 @@ defmodule Healthlocker.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.registration_changeset(%User{}, user_params)
+    changeset = User.changeset(%User{}, user_params)
+    
+    case Repo.insert(changeset) do
+      {:ok, _user} ->
+        render(conn, "signup2.html", changeset: changeset, action: :create2)
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def signup2(conn, _params) do
+    changeset = User.security_question(%User{})
+    render(conn, "signup2.html", changeset: changeset)
+  end
+
+  def create2(conn, %{"signup2" => signup2_params}) do
+    changeset = User.registration_changeset(%User{}, signup2_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
@@ -22,7 +38,7 @@ defmodule Healthlocker.UserController do
         |> put_flash(:info, "User created successfully.")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "signup.html", changeset: changeset)
     end
   end
 end
