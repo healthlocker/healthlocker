75e2a97ca4d169b48bc3cdfb16bbc9695f137dfd
katbow <katherine.bowler@gmail.com>
2017-02-09 15:47:41 +0000

    Add update route and restructure redirect #12

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -14,31 +14,50 @@ defmodule Healthlocker.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
-    
     case Repo.insert(changeset) do
-      {:ok, _user} ->
-        render(conn, "signup2.html", changeset: changeset, action: :create2)
+      {:ok, user} ->
+        conn
+        |> redirect(to: "/users/#{user.id}/signup2", action: :signup2, user: user)
+        # render(conn, "signup2.html", changeset: changeset, action: :update, user: user)
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
-  def signup2(conn, _params) do
+  def signup2(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
     changeset = User.security_question(%User{})
-    render(conn, "signup2.html", changeset: changeset)
+    IO.inspect changeset
+    render(conn, "signup2.html", changeset: changeset, action: "/users/#{user.id}/#{:update}")
   end
 
-  def create2(conn, %{"signup2" => signup2_params}) do
-    changeset = User.registration_changeset(%User{}, signup2_params)
+  def update(conn, %{"user" => user_params, "id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.registration_changeset(user, user_params)
 
-    case Repo.insert(changeset) do
+    case Repo.update(changeset) do
       {:ok, user} ->
         conn
         |> Healthlocker.Auth.login(user)
         |> put_flash(:info, "User created successfully.")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
-        render(conn, "signup.html", changeset: changeset)
+        render(conn, "signup2.html", changeset: changeset)
     end
   end
+
+  #
+  # def update(conn, %{"id" => id, "user" => user_params}) do
+  #   user = Repo.get!(User, id)
+  #   changeset = User.changeset(user, user_params)
+  #
+  #   case Repo.update(changeset) do
+  #     {:ok, user} ->
+  #       conn
+  #       |> put_flash(:info, "User updated successfully.")
+  #       |> redirect(to: user_path(conn, :show, user))
+  #     {:error, changeset} ->
+  #       render(conn, "edit.html", user: user, changeset: changeset)
+  #   end
+  # end
 end
