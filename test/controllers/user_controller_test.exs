cd1e3f503952c043d27c24b43ac2d93caad78749
katbow <katherine.bowler@gmail.com>
2017-02-10 18:09:25 +0000

    Add tests for rendering signup forms #12

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,14 +2,9 @@ defmodule Healthlocker.UserControllerTest do
   use Healthlocker.ConnCase
 
   alias Healthlocker.User
-  @valid_attrs %{
+  @step1_attrs %{
     email: "me@example.com",
-    password: "abc123",
-    name: "MyName",
-    security_question: "Favourite animal?",
-    security_answer: "Cat",
-    data_access: true,
-    role: "service user"
+    name: "MyName"
   }
   @invalid_attrs %{}
 
@@ -18,13 +13,37 @@ defmodule Healthlocker.UserControllerTest do
     assert html_response(conn, 200) =~ "Welcome! Get started by adding new content"
   end
 
-  test "renders form for new resources", %{conn: conn} do
+  test "renders form for new name and email", %{conn: conn} do
     conn = get conn, user_path(conn, :new)
     assert html_response(conn, 200) =~ "New user"
   end
 
+  test "renders form for new user password and security Q&A", %{conn: conn} do
+    case Repo.insert %User{
+      email: "me@example.com",
+      name: "MyName"
+    } do
+      {:ok, user} ->
+        conn = get conn, "/users/#{user.id}/signup2"
+    end
+    assert html_response(conn, 200) =~ "Password"
+  end
+
+  test "renders form for accepting T&Cs, privacy, and data access request", %{conn: conn} do
+    case Repo.insert %User{
+      email: "me@example.com",
+      password: "password",
+      security_question: "Favourite food?",
+      security_answer: "pizza"
+    } do
+      {:ok, user} ->
+        conn = get conn, "/users/#{user.id}/signup3"
+    end
+    assert html_response(conn, 200) =~ "terms and conditions"
+  end
+
   test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, user_path(conn, :create), user: @valid_attrs
+    conn = post conn, user_path(conn, :create), user: @step1_attrs
     user = Repo.get_by(User, email: "me@example.com")
     assert redirected_to(conn) == "/users/#{user.id}/signup2"
     assert user
@@ -34,5 +53,4 @@ defmodule Healthlocker.UserControllerTest do
     conn = post conn, user_path(conn, :create), user: @invalid_attrs
     assert html_response(conn, 200) =~ "New user"
   end
-
 end
