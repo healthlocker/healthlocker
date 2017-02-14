146b0aeb12f0835a6f0013e641740af1ab92c8fe
katbow <katherine.bowler@gmail.com>
2017-02-14 10:30:20 +0000

    Update migration to reference users table #88

--- priv/repo/migrations/20170213162440_add_post_belongs_to_user.exs
+++ priv/repo/migrations/20170213162440_add_post_belongs_to_user.exs
@@ -3,7 +3,7 @@ defmodule Healthlocker.Repo.Migrations.AddPostBelongsToUser do
 
   def change do
     alter table(:posts) do
-      add :user_id, :integer
+      add :user_id, references(:users)
     end
   end
 end
