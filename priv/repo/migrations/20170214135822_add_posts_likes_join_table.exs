c3aa56a107de00a4855feffb50017aa01f1e6dcf
katbow <katherine.bowler@gmail.com>
2017-02-14 20:29:20 +0000

    Add migration to add post_likes join table #102

---
+++ priv/repo/migrations/20170214135822_add_posts_likes_join_table.exs
@@ -0,0 +1,12 @@
+defmodule Healthlocker.Repo.Migrations.AddPostsLikesJoinTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts_likes, primary_key: false) do
+      add :post_id, references(:posts)
+      add :user_id, references(:users)
+
+      timestamps()
+    end
+  end
+end
