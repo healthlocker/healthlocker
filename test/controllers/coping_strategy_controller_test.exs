2fe8577638734770acf4a921dd99136015c096fb
katbow <katherine.bowler@gmail.com>
2017-02-16 16:44:53 +0000

    Add tests for new and create actions for coping strat controller #88 #98

---
+++ test/controllers/coping_strategy_controller_test.exs
@@ -0,0 +1,16 @@
+defmodule Healthlocker.CopingStrategyController do
+  use Healthlocker.ConnCase
+
+  @valid_attrs %{content: "some content"}
+
+  test "renders form for new name and email", %{conn: conn} do
+    conn = get conn, coping_strategy_path(conn, :new)
+    assert html_response(conn, 200) =~ "Add new"
+  end
+
+  test "creates resource and redirects when data is valid", %{conn: conn} do
+    conn = post conn, coping_strategy_path(conn, :create), post: @valid_attrs
+    assert redirected_to(conn) == coping_strategy_path(conn, :index)
+    assert Repo.get_by(Post, @valid_attrs)
+  end
+end
