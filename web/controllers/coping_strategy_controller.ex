63533a3054a19be1d314f306bb721f8ce744b932
katbow <katherine.bowler@gmail.com>
2017-02-16 16:45:23 +0000

    Add actions for rendering new html and creating new strategies #88 #98

---
+++ web/controllers/coping_strategy_controller.ex
@@ -0,0 +1,27 @@
+defmodule Healthlocker.CopingStrategyController do
+  use Healthlocker.Web, :controller
+
+  alias Healthlocker.Post
+
+  def new(conn, _params) do
+    changeset =  Post.changeset(%Post{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"post" => coping_strategy_params}) do
+    content = %{"content" => coping_strategy_params["content"] <> " #CopingStrategy"}
+    user_id = get_session(conn, :user_id)
+    changeset = Post.changeset(%Post{}, content)
+    changeset = Ecto.Changeset.put_change(changeset, :user_id, user_id)
+
+    case Repo.insert(changeset) do
+      {:ok, _post} ->
+        posts = Repo.all(Post)
+        conn
+        |> put_flash(:info, "Coping strategy added!")
+        |> redirect(to: coping_strategy_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end
