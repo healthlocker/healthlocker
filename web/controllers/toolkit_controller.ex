68ba701001a1ad1870b4cbd69b99c9eb57d781ef
katbow <katherine.bowler@gmail.com>
2017-02-17 11:00:03 +0000

    Get 3 coping strategies to display in toolkit #88 #99

--- web/controllers/toolkit_controller.ex
+++ web/controllers/toolkit_controller.ex
@@ -1,7 +1,12 @@
 defmodule Healthlocker.ToolkitController do
   use Healthlocker.Web, :controller
 
+  alias Healthlocker.Post
+
   def index(conn, _params) do
-    render conn, "index.html"
+    coping_strategies = Post
+                        |> Post.get_coping_strategies
+                        |> Repo.all
+    render conn, "index.html", coping_strategies: coping_strategies
   end
 end
