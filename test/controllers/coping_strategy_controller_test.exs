09f7157ce8a06e69005fbf91a874e9272e385db4
katbow <katherine.bowler@gmail.com>
2017-02-17 15:55:37 +0000

    Add tests for rendering edit form and updating strategy #37 #88

--- test/controllers/coping_strategy_controller_test.exs
+++ test/controllers/coping_strategy_controller_test.exs
@@ -16,7 +16,7 @@ defmodule Healthlocker.CopingStrategyController do
     end
   end
 
-  test "renders form for new name and email", %{conn: conn} do
+  test "renders form for new coping strategy", %{conn: conn} do
     conn = get conn, coping_strategy_path(conn, :new)
     assert html_response(conn, 200) =~ "Add new"
   end
@@ -25,5 +25,19 @@ defmodule Healthlocker.CopingStrategyController do
     conn = post conn, coping_strategy_path(conn, :create), post: @valid_attrs
     assert redirected_to(conn) == coping_strategy_path(conn, :index)
     assert Repo.get_by(Post, @valid_attrs)
+  end
+
+  test "renders form for editing coping strategy", %{conn: conn} do
+    Repo.insert %Post{content: "some content"}
+    coping_strategy = Repo.get_by(Post, content: "some content")
+    conn = get conn, coping_strategy_path(conn, :edit, coping_strategy: coping_strategy)
+    assert html_response(conn, 200) =~ "Edit coping strategy"
+  end
+
+  test "updates coping strategy", %{conn: conn} do
+    Repo.insert %Post{content: "some content"}
+    coping_strategy = Repo.get_by(Post, content: "some content")
+    conn = put conn, coping_strategy_path(conn, :update, coping_strategy: coping_strategy)
+    assert redirected_to(conn) == coping_strategy_path(conn, :show, coping_strategy: coping_strategy)
   end
 end
