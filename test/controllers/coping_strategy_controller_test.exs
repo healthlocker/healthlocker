267dd1bed326611beda3d5237bc4d46c95978c98
katbow <katherine.bowler@gmail.com>
2017-02-17 12:04:57 +0000

    Add test for coping strategy index #88 #99

--- test/controllers/coping_strategy_controller_test.exs
+++ test/controllers/coping_strategy_controller_test.exs
@@ -5,6 +5,11 @@ defmodule Healthlocker.CopingStrategyController do
 
   @valid_attrs %{content: "some content"}
 
+  test "renders index.html on /coping-strategy", %{conn: conn} do
+    conn = get conn, coping_strategy_path(conn, :index)
+    assert html_response(conn, 200) =~ "Coping strategies"
+  end
+
   test "renders form for new name and email", %{conn: conn} do
     conn = get conn, coping_strategy_path(conn, :new)
     assert html_response(conn, 200) =~ "Add new"
