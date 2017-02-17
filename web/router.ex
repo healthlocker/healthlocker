23b0438e2a9793531a7ad54d552ebf0b49b619e6
katbow <katherine.bowler@gmail.com>
2017-02-17 12:10:50 +0000

    Add route for coping strategy index #37 #88

--- web/router.ex
+++ web/router.ex
@@ -27,7 +27,7 @@ defmodule Healthlocker.Router do
     get "/users/:id/signup3", UserController, :signup3
     put "/users/:id/create3", UserController, :create3
     resources "/login", LoginController, only: [:index, :create, :delete]
-    resources "/coping-strategy", CopingStrategyController, only: [:new, :create]
+    resources "/coping-strategy", CopingStrategyController, only: [:index, :new, :create]
     resources "/toolkit", ToolkitController, only: [:index]
 
   end
