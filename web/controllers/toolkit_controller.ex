f78f2a80b2aae965485b1bd4cb421354627f7c2e
katbow <katherine.bowler@gmail.com>
2017-02-20 11:47:26 +0000

    Add check for if current user matches in posts for toolkit #37 #88

--- web/controllers/toolkit_controller.ex
+++ web/controllers/toolkit_controller.ex
@@ -4,8 +4,9 @@ defmodule Healthlocker.ToolkitController do
   alias Healthlocker.Post
 
   def index(conn, _params) do
+    user_id = conn.assigns.current_user.id
     coping_strategies = Post
-                        |> Post.get_coping_strategies
+                        |> Post.get_coping_strategies(user_id)
                         |> Repo.all
                         |> Enum.take(-3)
     render conn, "index.html", coping_strategies: coping_strategies
