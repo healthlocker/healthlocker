a1477c5c031aafb7e7f34288031393388dd2e165
katbow <katherine.bowler@gmail.com>
2017-02-22 14:54:29 +0000

    Add current user for posts

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -3,14 +3,20 @@ defmodule Healthlocker.PostControllerTest do
   import Healthlocker.Fixtures
 
   alias Healthlocker.Post
+  alias Healthlocker.User
   @valid_attrs %{content: "some content #story"}
   @invalid_attrs %{}
 
-  # setup do
-  #   user = insert_user()
-  #   conn = assign(build_conn(), :current_user, user)
-  #   {:ok, conn: conn, user: user}
-  # end
+  setup do
+    %User{
+      id: 123456,
+      name: "MyName",
+      email: "abc@gmail.com",
+      password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+    } |> Repo.insert
+
+    {:ok, user: Repo.get(User, 123456) }
+  end
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, post_path(conn, :index)
@@ -32,21 +38,27 @@ defmodule Healthlocker.PostControllerTest do
     assert html_response(conn, 302)
   end
 
-  # test "renders form for new resources", %{conn: conn, user: user} do
-  #   conn = get conn, post_path(conn, :new)
-  #   assert html_response(conn, 200) =~ "Post a story or tip"
-  # end
+  test "renders form for new resources", %{conn: conn, user: user} do
+    conn = build_conn()
+        |> assign(:current_user, user)
+        |> get(post_path(conn, :new))
+    assert html_response(conn, 200) =~ "Post a story or tip"
+  end
 
-  # test "creates resource and redirects when data is valid", %{conn: conn} do
-  #   conn = post conn, post_path(conn, :create), post: @valid_attrs
-  #   assert redirected_to(conn) == post_path(conn, :index)
-  #   assert Repo.get_by(Post, @valid_attrs)
-  # end
-  #
-  # test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-  #   conn = post conn, post_path(conn, :create), post: @invalid_attrs
-  #   assert html_response(conn, 200) =~ "Post a story or tip"
-  # end
+  test "creates resource and redirects when data is valid", %{conn: conn, user: user} do
+    conn = build_conn()
+        |> assign(:current_user, user)
+        |> post(post_path(conn, :create), post: @valid_attrs)
+    assert redirected_to(conn) == post_path(conn, :new)
+    assert Repo.get_by(Post, @valid_attrs)
+  end
+
+  test "does not create resource and renders errors when data is invalid", %{conn: conn, user: user} do
+    conn = build_conn()
+        |> assign(:current_user, user)
+        |> post(post_path(conn, :create), post: @invalid_attrs)
+    assert html_response(conn, 200) =~ "Post a story or tip"
+  end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
     assert_error_sent 404, fn ->
