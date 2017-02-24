3aaa019368ced46bd8b068d654079f3402f78cad
katbow <katherine.bowler@gmail.com>
2017-02-24 12:40:12 +0000

    Add action to render edit password form #28 #127

--- web/controllers/account_controller.ex
+++ web/controllers/account_controller.ex
@@ -37,7 +37,7 @@ defmodule Healthlocker.AccountController do
     user = Repo.get!(User, user_id)
     changeset = User.security_question(%User{})
     render conn, "edit_security.html", changeset: changeset, user: user,
-                                       action: "/account/update-security"
+                                       action: account_path(conn, :update_security)
   end
 
   def update_security(conn, %{"user" => user_params}) do
@@ -46,7 +46,7 @@ defmodule Healthlocker.AccountController do
 
     if user_params["security_check"] == user.security_answer do
       changeset = User.security_question(user, user_params)
-      IO.inspect changeset
+
       case Repo.update(changeset) do
         {:ok, _params} ->
           conn
@@ -60,6 +60,19 @@ defmodule Healthlocker.AccountController do
       |> put_flash(:error, "Security answer does not match")
       |> redirect(to: account_path(conn, :edit_security))
     end
+  end
+
+  def edit_password(conn, _params) do
+    user_id = conn.assigns.current_user.id
+    user = Repo.get!(User, user_id)
+    changeset = User.security_question(%User{})
+    render conn, "edit_password.html", changeset: changeset, user: user,
+                                       action: account_path(conn, :update_password)
+  end
+
+  def update_password(conn, %{"user" => user_params}) do
+    user_id = conn.assigns.current_user.id
+    user = Repo.get!(User, user_id)
   end
 
   defp authenticate(conn, _opts) do
