1426a6c0cf5175ef15032425887bde88b4ea9cf7
katbow <katherine.bowler@gmail.com>
2017-03-03 15:35:46 +0000

    Add migration to create slam_user table #138

---
+++ priv/repo/migrations/20170303152052_create_slam_user.exs
@@ -0,0 +1,13 @@
+defmodule Healthlocker.Repo.Migrations.CreateSlamUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:slam_users) do
+     add :first_name, :string
+     add :last_name, :string
+     add :email, :string
+     add :nhs_number, :string
+     add :phone_number, :string
+   end
+  end
+end
