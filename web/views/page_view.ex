f184720d2ca13c46e9765f7ce8128380954391db
katbow <katherine.bowler@gmail.com>
2017-03-05 15:47:45 +0000

    Move function to get tag into views #108

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -6,5 +6,15 @@ defmodule Healthlocker.PageView do
     |> Earmark.as_html!
     |> raw
   end
-  
+
+  def tip_tag(content) do
+    cond do
+      String.contains?(String.downcase(content), "#connect") -> "Connect"
+      String.contains?(String.downcase(content), "#keeplearning") -> "KeepLearning"
+      String.contains?(String.downcase(content), "#givetoothers") -> "GiveToOthers"
+      String.contains?(String.downcase(content), "#beactive") -> "BeActive"
+      String.contains?(String.downcase(content), "#takenotice") -> "TakeNotice"
+      true -> "no tag found"
+    end
+  end
 end
