e912202d50050c384799ab00a192370abbb90378
katbow <katherine.bowler@gmail.com>
2017-03-07 10:03:21 +0000

    Add slam user id to users table #138

--- priv/repo/migrations/20170303155733_add_slam_connected_to_users.exs
+++ priv/repo/migrations/20170303155733_add_slam_connected_to_users.exs
@@ -3,7 +3,7 @@ defmodule Healthlocker.Repo.Migrations.AddSlamConnectedToUsers do
 
   def change do
     alter table(:users) do
-      add :slam_connected, :boolean
+      add :slam_user_id, references(:slam_users)
     end
   end
 end
