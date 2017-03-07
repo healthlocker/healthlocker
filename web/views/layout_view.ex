35ed528fc83286bca2fd8e668eff61ac229bb51c
Tony Daly <tony.daly@me.com>
2017-03-07 15:49:01 +0000

    Render the segment snippet when we have an api key.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,11 @@
 defmodule Healthlocker.LayoutView do
   use Healthlocker.Web, :view
+
+  def segment_snippet do
+    if segment_api_key, do: render(Healthlocker.LayoutView, "_segment.html")
+  end
+
+  defp segment_api_key do
+    System.get_env("SEGMENT_API_KEY")
+  end
 end
