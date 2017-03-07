6e6590e65d6b64cbe3bf202916be73d68abb391f
Tony Daly <tony.daly@me.com>
2017-03-07 19:16:35 +0000

    Remove console.logs

--- web/static/js/segment.js
+++ web/static/js/segment.js
@@ -1,9 +1,5 @@
 export var Segment = {
   identify: function(id, name, email) {
-    console.log("identify: " + id);
-    console.log("identify: " + name);
-    console.log("identify: " + email);
-
     analytics.identify(id, {
       name: name,
       email: email
