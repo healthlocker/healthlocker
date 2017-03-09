f67ff8ed61fba251c778d742aab77af631ffd18b
katbow <katherine.bowler@gmail.com>
2017-03-09 15:38:33 +0000

    Move logic to goal view #212

--- web/views/goal_view.ex
+++ web/views/goal_view.ex
@@ -1,9 +1,14 @@
 defmodule Healthlocker.GoalView do
   use Healthlocker.Web, :view
+  use Timex
 
   def markdown(body) do
     body
     |> Earmark.as_html!
     |> raw
+  end
+
+  def pretty_date(date) do
+    Date.to_iso8601(date)
   end
 end
