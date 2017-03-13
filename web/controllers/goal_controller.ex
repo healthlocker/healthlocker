0fc833b67aabdf510e97bf5fa1a35c3d8418099f
katbow <katherine.bowler@gmail.com>
2017-03-13 12:42:49 +0000

    Sort goals by most recently updated & important #214

--- web/controllers/goal_controller.ex
+++ web/controllers/goal_controller.ex
@@ -14,7 +14,12 @@ defmodule Healthlocker.GoalController do
       conn
       |> redirect(to: goal_path(conn, :new))
     else
-      render conn, "index.html", goals: goals
+      important_goals =  Enum.filter(goals, fn(g) -> g.important end)
+      |> Enum.sort(&(&1.updated_at > &2.updated_at))
+      unimportant_goals = Enum.filter(goals, fn(g) -> !g.important end)
+      |> Enum.sort(&(&1.updated_at > &2.updated_at))
+      all_goals = Enum.concat(important_goals, unimportant_goals)
+      render conn, "index.html", goals: all_goals
     end
   end
 
@@ -28,19 +33,18 @@ defmodule Healthlocker.GoalController do
     goal = Goal
           |> Goal.get_goal_by_user(id, user_id)
           |> Repo.one!
-    mark_important_changeset = Goal.mark_important_changeset(goal)
     render conn, "show.html", goal: goal
   end
 
   def mark_important(conn, %{"id" => id}) do
     goal = Repo.get!(Goal, id)
-      changeset = Goal.mark_important_changeset(goal, %{important: !goal.important})
+    changeset = Goal.mark_important_changeset(goal, %{important: !goal.important})
 
     case Repo.update(changeset) do
       {:ok, goal} ->
         conn
         |> redirect(to: goal_path(conn, :show, goal))
-      {:error, changeset} ->
+      {:error, _changeset} ->
         conn
         |> put_flash(:info, "Could not mark as important. Try again later.")
         |> render("show.html", goal: goal)
