bd504df2032bec7be593ff533e9174cfda4de9af
katbow <katherine.bowler@gmail.com>
2017-03-17 17:42:35 +0000

    Add function to render messages #49 #283 #284

--- web/views/message_view.ex
+++ web/views/message_view.ex
@@ -1,3 +1,11 @@
 defmodule Healthlocker.MessageView do
   use Healthlocker.Web, :view
+
+  def render("message.json", %{message: msg}) do
+    %{
+      id: msg.id,
+      body: msg.body,
+      name: msg.name
+    }
+  end
 end
