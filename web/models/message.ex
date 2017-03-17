637dc6de793ac819d5eb38a3ccc63ca4def27660
katbow <katherine.bowler@gmail.com>
2017-03-17 15:40:59 +0000

    Add schema for  message model #49 #283 #284

---
+++ web/models/message.ex
@@ -0,0 +1,19 @@
+defmodule Healthlocker.Message do
+  use Healthlocker.Web, :model
+
+  schema "messages" do
+    field :body, :string
+    belongs_to :user, Healthlocker.User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:body])
+    |> validate_required([:body])
+  end
+end
