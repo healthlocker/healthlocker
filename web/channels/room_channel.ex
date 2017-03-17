cb94d8e17f0ab607a3803db78339674d7f5c6910
katbow <katherine.bowler@gmail.com>
2017-03-17 16:25:41 +0000

    Change handle_in functions to ensure events have current user and add messages to db #49 #283 #284

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,14 +1,33 @@
 defmodule Healthlocker.RoomChannel do
   use Healthlocker.Web, :channel
 
+  alias Healthlocker.Message
+
   def join("room:general", message, socket) do
     Process.flag(:trap_exit, true)
     send(self, {:after_join, message})
     {:ok, socket}
   end
 
-  def handle_in("new:msg", msg, socket) do
-    broadcast! socket, "new:msg", %{user: msg["user"], body: msg["body"]}
-    {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :user, msg["user"])}
+  def handle_in(event, params, socket) do
+    user = Repo.get(Healthlocker.User, socket.assigns.user_id)
+    handle_in(event, params, user, socket)
+  end
+
+  def handle_in("new:msg", msg, user, socket) do
+    changeset = Message.changeset(%Message{
+        body: msg["body"],
+        user_id: user.id
+      })
+    case Repo.insert(changeset) do
+      {:ok, _message} ->
+        broadcast! socket, "new:msg", %{
+          user: msg["user"],
+          body: msg["body"]
+        }
+        {:reply, {:ok, %{msg: msg["body"]}}, socket}
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end
