09018dc375c4229ba3587fefc87dc432540a3b1e
katbow <katherine.bowler@gmail.com>
2017-03-17 15:06:07 +0000

    Add token to socket params #49 #283 #284

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -5,8 +5,7 @@
 // and connect at the socket path in 'lib/my_app/endpoint.ex':
 import {Socket} from 'phoenix'
 
-let socket = new Socket('/socket') //, {params: {token: window.userToken}})
-
+let socket = new Socket('/socket', {params: {token: window.userToken}})
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
 // which authenticates the session and assigns a `:current_user`.
