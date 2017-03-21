1d0cb1c0604c727f9e91a1756fa54fe4c21dc0b7
katbow <katherine.bowler@gmail.com>
2017-03-21 15:05:05 +0000

    Add function for making security questions in view #144

--- web/views/component_view.ex
+++ web/views/component_view.ex
@@ -1,3 +1,27 @@
 defmodule Healthlocker.ComponentView do
   use Healthlocker.Web, :view
+  @root_dir File.cwd!
+  @questions_raw Path.join(~w(#{@root_dir} web/templates security_questions.txt))
+
+  def security_questions do
+    # load security questions from file
+    questions = @questions_raw |> File.read!
+
+    # split on line breaks to separate the questions:
+    questions_array = String.split(questions, "\n")
+
+    # create a map from the questions array
+    questions_map = %{}
+    # create an array where "question: question"
+
+    # gets a map which displays in template, except there is an
+    # empty string at the beginning
+    map = Enum.reduce questions_array, %{}, fn x, acc ->
+      Map.put(acc, x, x)
+    end
+
+    # return the questions map
+    map
+  end
+
 end
