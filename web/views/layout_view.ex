49894a5699b3e7e38c005ffd59c5696ea9cbe163
Tony Daly <tony.daly@me.com>
2017-03-21 17:42:17 +0000

    Rename SEGMENT_API_KEY to SEGMENT_WRITE_KEY.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -2,10 +2,10 @@ defmodule Healthlocker.LayoutView do
   use Healthlocker.Web, :view
 
   def segment_snippet do
-    if segment_api_key, do: render(Healthlocker.LayoutView, "_segment.html")
+    if segment_write_key, do: render(Healthlocker.LayoutView, "_segment.html")
   end
 
-  defp segment_api_key do
-    System.get_env("SEGMENT_API_KEY")
+  defp segment_write_key do
+    System.get_env("SEGMENT_WRITE_KEY")
   end
 end
