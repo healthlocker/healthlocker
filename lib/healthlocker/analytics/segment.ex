2e11c286bf0e8a730f88b2f1d0b6057a33f847e4
Tony Daly <tony.daly@me.com>
2017-03-21 18:58:32 +0000

    Remove logging.

--- lib/healthlocker/analytics/segment.ex
+++ lib/healthlocker/analytics/segment.ex
@@ -2,14 +2,12 @@ defmodule Healthlocker.Analytics.Segment do
   @moduledoc """
   Interface to the Segment API via [`analytics-elixir`](https://github.com/stueccles/analytics-elixir)
   """
-  require Logger
 
   def identify(user_id, traits) do
     Segment.Analytics.identify(user_id, traits)
   end
 
   def track(user_id, event_name, properties) do
-    Logger.info "Segment track"
     Segment.Analytics.track(user_id, event_name, properties)
   end
 end
