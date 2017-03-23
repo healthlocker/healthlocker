8a92d5dc49e6ff49eb83e9840a26cfe0f0a662c8
katbow <katherine.bowler@gmail.com>
2017-03-23 16:33:44 +0000

    Render data from list to maintain order #144

--- web/views/component_view.ex
+++ web/views/component_view.ex
@@ -9,17 +9,10 @@ defmodule Healthlocker.ComponentView do
     questions = @questions_raw |> File.read!
 
     # split on line breaks to separate the questions:
-    questions_array = String.split(questions, "\n")
+    questions_list = String.split(questions, "\n")
+                    |> List.delete("")
 
-    # gets a map which displays in template, except there is an
-    # empty string at the beginning
-    map = Enum.reduce questions_array, %{}, fn x, acc ->
-      Map.put(acc, x, x)
-    end
-
-    # delete any empty string "" keys
-    questions_map = Map.delete(map, map[""])
-    # return the questions map
-    questions_map
+    # return the questions list
+    questions_list
   end
 end
