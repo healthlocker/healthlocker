6c37d7841887e4006361ac10ee353363a5ddcf5e
katbow <katherine.bowler@gmail.com>
2017-03-23 14:42:40 +0000

    Change table name and data types #320

--- priv/repo/migrations/20170323111602_create_sleep_tracker.exs
+++ priv/repo/migrations/20170323111602_create_sleep_tracker.exs
@@ -2,15 +2,15 @@ defmodule Healthlocker.Repo.Migrations.CreateSleepTracker do
   use Ecto.Migration
 
   def change do
-    create table(:sleeptrackers) do
-      add :hours_slept, :integer
-      add :wake_count, :integer
+    create table(:sleep_trackers) do
+      add :hours_slept, :string
+      add :wake_count, :string
       add :notes, :string
-      add :user_id, references(:users, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :delete_all)
 
       timestamps()
     end
-    create index(:sleeptrackers, [:user_id])
+    create index(:sleep_trackers, [:user_id])
 
   end
 end
