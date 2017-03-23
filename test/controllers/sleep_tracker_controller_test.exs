66d776ce946c36790d8d01f893754e5e0b38d9b1
katbow <katherine.bowler@gmail.com>
2017-03-23 15:00:41 +0000

    Add tests for each route without current user #320

--- test/controllers/sleep_tracker_controller_test.exs
+++ test/controllers/sleep_tracker_controller_test.exs
@@ -10,13 +10,13 @@ defmodule Healthlocker.SleepTrackerControllerTest do
     user_id: 123456
   }
 
-  describe "tests with current user" do
+  describe "with current user" do
     setup do
       %User{
-        id: 123456,
-        name: "MyName",
-        email: "abc@gmail.com",
-        password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+          id: 123456,
+          name: "MyName",
+          email: "abc@gmail.com",
+          password_hash: Comeonin.Bcrypt.hashpwsalt("password")
         } |> Repo.insert
 
         {:ok, conn: build_conn() |> assign(:current_user, Repo.get(User, 123456)) }
@@ -37,6 +37,37 @@ defmodule Healthlocker.SleepTrackerControllerTest do
       sleep_tracker = Repo.get_by(SleepTracker, notes: "Some notes")
       assert redirected_to(conn) == toolkit_path(conn, :index)
       assert sleep_tracker
+    end
+  end
+
+  describe "without current user" do
+    setup do
+      %User{
+          id: 123456,
+          name: "MyName",
+          email: "abc@gmail.com",
+          password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+        } |> Repo.insert
+
+      :ok
+    end
+
+    test "conn is halted for index", %{conn: conn} do
+      conn = get conn, sleep_tracker_path(conn, :index)
+      assert html_response(conn, 302)
+      assert conn.halted
+    end
+
+    test "conn is halted for new", %{conn: conn} do
+      conn = get conn, sleep_tracker_path(conn, :new)
+      assert html_response(conn, 302)
+      assert conn.halted
+    end
+
+    test "conn is halted for create", %{conn: conn} do
+      conn = post conn, sleep_tracker_path(conn, :create)
+      assert html_response(conn, 302)
+      assert conn.halted
     end
   end
 end
