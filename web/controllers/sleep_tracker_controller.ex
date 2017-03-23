73360c5e3aa4582e33bf307c489f68f65e7f5494
katbow <katherine.bowler@gmail.com>
2017-03-23 11:54:42 +0000

    Add action for new sleep tracker #320

--- web/controllers/sleep_tracker_controller.ex
+++ web/controllers/sleep_tracker_controller.ex
@@ -1,7 +1,14 @@
 defmodule Healthlocker.SleepTrackerController do
   use Healthlocker.Web, :controller
 
+  alias Healthlocker.SleepTracker
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
+  end
+
+  def new(conn, %{"user_params" => user_params}) do
+    changeset = SleepTracker.changeset(%User{})
+    render(conn, "new.html", changeset: changeset)
   end
 end
