fc78d4bfd3f6ced50ae4f8009459128141945123
katbow <katherine.bowler@gmail.com>
2017-03-28 09:42:46 +0100

    Remove hours_slept function #333

--- web/views/sleep_tracker_view.ex
+++ web/views/sleep_tracker_view.ex
@@ -2,21 +2,8 @@ defmodule Healthlocker.SleepTrackerView do
   use Healthlocker.Web, :view
 
   @root_dir File.cwd!
-  @hours_slept_raw Path.join(~w(#{@root_dir} web/templates hours_slept.txt))
 
   @wake_count_raw Path.join(~w(#{@root_dir} web/templates wake_count.txt))
-
-  def hours_slept do
-    # load hours_slept from file
-    hours_slept = @hours_slept_raw |> File.read!
-
-    # split on line breaks to separate the hours_slept:
-    hours_slept_list = String.split(hours_slept, "\n")
-                    |> List.delete("")
-
-    # return the hours_slept list
-    hours_slept_list
-  end
 
   def wake_count do
     # load wake_count from file
