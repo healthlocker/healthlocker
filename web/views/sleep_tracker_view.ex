246723d3bdad91a6f3432ad9bbee154c118420a4
katbow <katherine.bowler@gmail.com>
2017-03-28 09:47:17 +0100

    Remove function for wake count in sleep tracker view #333

--- web/views/sleep_tracker_view.ex
+++ web/views/sleep_tracker_view.ex
@@ -1,19 +1,3 @@
 defmodule Healthlocker.SleepTrackerView do
   use Healthlocker.Web, :view
-
-  @root_dir File.cwd!
-
-  @wake_count_raw Path.join(~w(#{@root_dir} web/templates wake_count.txt))
-
-  def wake_count do
-    # load wake_count from file
-    wake_count = @wake_count_raw |> File.read!
-
-    # split on line breaks to separate the wake_count:
-    wake_count_list = String.split(wake_count, "\n")
-                    |> List.delete("")
-
-    # return the wake_count list
-    wake_count_list
-  end
 end
