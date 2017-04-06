e3ea08ab3af4fef30c35e2ac4f7fc42038de736a
Cleop <cleo.pearson@hotmail.co.uk>
2017-04-06 16:21:22 +0100

    Creates test for accepting the terms of service on sign up

---
+++ test/acceptance_tests/SU_accept_terms_required_test.js
@@ -0,0 +1,26 @@
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
+// Homepage > Sign up > pw and security q > do not accept terms of service - should receive 'must be accepted' error message
+
+nightmare
+  .goto('https://www.healthlocker.uk/')
+  .click('main#body > section.h-100.pa3.pa5-ns.bt.b--black-10.bg-white:nth-child(1) > section.mw7.center:nth-child(1) > section.bb.b--black-20.pb3.ph3.center:nth-child(3) > p:nth-child(4) > a:nth-child(1)')
+  .click('input#user_email')
+  .type('input#user_email', 'test2@testing.com')
+  .click('main#body > section.h-100.pa3.pa5-ns.bt.b--black-10.bg-white:nth-child(1) > section.mw7.center:nth-child(1) > div.ma3.ma4-m.ma4-l:nth-child(3) > div.br2.mv3.center:nth-child(3) > form:nth-child(1) > div.tr:nth-child(5) > button.mt4.f5.link.dim.br-pill.ph3.pv2.dib.black-60.hl-bg-grey.pointer.w-40:nth-child(1)')
+  .wait(1000)
+  .click('input#user_privacy')
+  .click('main#body > section.h-100.pa3.pa5-ns.bt.b--black-10.bg-white:nth-child(1) > section.mw7.center:nth-child(1) > div.ma3.ma4-m.ma4-l.mv2.mv3-m.mv4-l:nth-child(3) > form:nth-child(4) > div.tr:nth-child(7) > button.mt4.f5.link.dim.br-pill.ph3.pv2.dib.black-60.hl-bg-grey.pointer.w-40:nth-child(1)')
+  .wait(1000)
+  .screenshot('./test/acceptance_tests/screenshots/SU3_terms_not_accepted.png')
+  .evaluate(function(){
+    return document.getElementsByTagName('span')[0].innerHTML;
+  })
+  .end()
+    .then(function (result) {
+      result == `must be accepted` ? console.log(`PASS: error message given for failing to accept terms of service`) : console.log(`FAIL: did not correctly give error message for not agreeing to the terms of service`);
+    })
+    .catch(function (error) {
+      console.error('Error:', error);
+    });
