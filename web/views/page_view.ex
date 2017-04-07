01e866e2458a43f73bf53b8161e6345ef4c7f50b
Sam Houston <shouston3@me.com>
2017-04-07 14:21:34 +0100

    putting the markdown function in component_view and importing it where is it needed

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,20 +1,4 @@
 defmodule Healthlocker.PageView do
   use Healthlocker.Web, :view
-
-  def markdown(body) do
-    body
-    |> Earmark.as_html!
-    |> raw
-  end
-
-  def tip_tag(content) do
-    cond do
-      String.contains?(String.downcase(content), "#connect") -> "Connect"
-      String.contains?(String.downcase(content), "#keeplearning") -> "KeepLearning"
-      String.contains?(String.downcase(content), "#givetoothers") -> "GiveToOthers"
-      String.contains?(String.downcase(content), "#beactive") -> "BeActive"
-      String.contains?(String.downcase(content), "#takenotice") -> "TakeNotice"
-      true -> "no tag found"
-    end
-  end
+  import Healthlocker.ComponentView, only: [markdown: 1]
 end
--- web/views/tip_view.ex
+++ web/views/tip_view.ex
@@ -1,42 +1,4 @@
 defmodule Healthlocker.TipView do
   use Healthlocker.Web, :view
-
-  @tips ["Connect", "KeepLearning", "GiveToOthers", "BeActive", "TakeNotice"]
-
-  def markdown(body) do
-    body
-    |> Earmark.as_html!
-    |> wrap_tags
-    |> raw
-  end
-
-  defp handle_url(tip) do
-    cond do
-      String.downcase(tip) == "#tip" -> ""
-      true -> "?tag=#{tip}"
-    end
-  end
-
-  defp add_link(tip, html) do
-    html
-    |> String.split(tip)
-    |> Enum.join("<a href=\"/tips#{handle_url(tip)}\">#{tip}</a>")
-  end
-
-  defp add_links(html) do
-    ["Tip" | @tips] |> Enum.reduce(html, &add_link/2)
-  end
-
-  defp add_links_first_elem([empty_str | [head_html | tail_html]]) do
-    [empty_str | [add_links(head_html) | tail_html]]
-  end
-
-  defp wrap_tags(content) do
-    content
-    |> String.split("\n")
-    |> Enum.reverse
-    |> add_links_first_elem
-    |> Enum.reverse
-    |> Enum.join("\n")
-  end
+  import Healthlocker.ComponentView, only: [markdown: 1]
 end
