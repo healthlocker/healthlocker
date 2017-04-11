2e1f706a21b4db5199b22e7a1d70ab6603935cc6
Tony Daly <tony.daly@me.com>
2017-04-11 18:16:56 +0100

    Refactor button component naming.

--- web/views/component_helpers/button.ex
+++ web/views/component_helpers/button.ex
@@ -1,24 +1,24 @@
 defmodule Healthlocker.ComponentHelpers.Button do
   use Phoenix.HTML
 
-  def button_base, do: "f5 link dim dib mb4 ph3 pv2 br-pill hl-dark-blue pointer"
-  def button_primary, do: button_base() <> " " <> "hl-bg-yellow"
-  def button_secondary, do: button_base() <> " " <> "ba bw1 b--hl-yellow bg-white"
-  def button_inactive, do: button_base() <> " " <> "hl-bg-grey"
+  @base_classes "f5 link dim dib mb4 ph3 pv2 br-pill hl-dark-blue pointer"
+  def primary_classes, do: @base_classes <> " " <> "hl-bg-yellow"
+  def secondary_classes, do: @base_classes <> " " <> "ba bw1 b--hl-yellow bg-white"
+  def inactive_classes, do: @base_classes <> " " <> "hl-bg-grey"
 
-  def link_primary(text, opts) do
-    link(text, [class: button_primary()] ++ opts)
+  def button_primary(text, opts) do
+    link(text, [class: primary_classes] ++ opts)
   end
 
-  def link_secondary(text, opts) do
-    link(text, [class: button_secondary()] ++ opts)
+  def button_secondary(text, opts) do
+    link(text, [class: secondary_classes] ++ opts)
   end
 
-  def link_inactive(text, opts) do
-    link(text, [class: button_inactive()] ++ opts)
+  def button_inactive(text, opts) do
+    link(text, [class: inactive_classes] ++ opts)
   end
 
   def submit_primary(value, opts \\ []) do
-    submit(value, [class: button_primary()] ++ opts)
+    submit(value, [class: primary_classes] ++ opts)
   end
 end
