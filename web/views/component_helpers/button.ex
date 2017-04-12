8c3ea23b6a893b742982bc7467867d2c740b42d8
Tony Daly <tony.daly@me.com>
2017-04-12 15:27:13 +0100

    Update templates to use components.

--- web/views/component_helpers/button.ex
+++ web/views/component_helpers/button.ex
@@ -1,20 +1,20 @@
 defmodule Healthlocker.ComponentHelpers.Button do
   use Phoenix.HTML
 
-  @base_classes "f5 link dim dib mb4 ph3 pv2 br-pill hl-dark-blue pointer"
+  @base_classes "f5 link dim dib ph3 pv2 br-pill hl-dark-blue pointer"
   def primary_classes, do: @base_classes <> " " <> "hl-bg-yellow"
   def secondary_classes, do: @base_classes <> " " <> "ba bw1 b--hl-yellow bg-white"
   def inactive_classes, do: @base_classes <> " " <> "hl-bg-grey"
 
-  def button_primary(text, opts) do
-    link(text, [class: primary_classes] ++ opts)
+  def link_primary(text, opts) do
+    link(text, [class: primary_classes()] ++ opts)
   end
 
-  def button_secondary(text, opts) do
-    link(text, [class: secondary_classes] ++ opts)
+  def link_secondary(text, opts) do
+    link(text, [class: secondary_classes()] ++ opts)
   end
 
-  def button_inactive(text, opts) do
+  def link_inactive(text, opts) do
     link(text, [class: inactive_classes] ++ opts)
   end
 
