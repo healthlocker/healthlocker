2761338ca8070b2551075703c6f192f7cf089df3
katbow <katherine.bowler@gmail.com>
2017-04-18 17:10:47 +0100

    Fix test for adding styling to #tip and category #370

--- web/views/component_view.ex
+++ web/views/component_view.ex
@@ -46,8 +46,7 @@ defmodule Healthlocker.ComponentView do
   defp add_link(tip, html) do
     html
     |> String.split(tip)
-    |> Enum.join("<a href=\"/tips#{handle_url(tip)}\"
-        class=\"link hl-aqua underline hl-link\">#{tip}</a>")
+    |> Enum.join("<a href=\"/tips#{handle_url(tip)}\" class=\"link hl-aqua underline hl-link\">#{tip}</a>")
   end
 
   defp add_links(html) do
