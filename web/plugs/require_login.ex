e3c3fc036d0dd35dad258451fc69f87de9c64ea8
Tony Daly <tony.daly@me.com>
2017-04-19 16:01:11 +0100

    RequireLogin plug and tests.

--- web/plugs/require_login.ex
+++ web/plugs/require_login.ex
@@ -1,8 +1,20 @@
 defmodule Healthlocker.Plugs.RequireLogin do
   import Plug.Conn
+  use Healthlocker.Web, :controller
 
   def init(opts), do: opts
   def call(conn, _) do
+    if conn.assigns[:current_user] do
+      conn
+    else
+      conn |> redirect_to_login
+    end
+  end
+
+  defp redirect_to_login(conn) do
     conn
+    |> put_flash(:error,  "You must be logged in to access that page!")
+    |> redirect(to: login_path(conn, :index))
+    |> halt
   end
 end
