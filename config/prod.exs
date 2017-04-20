4d90f40d5dfb6fb519ee70d2e8eb0ca9371b63bc
katbow <katherine.bowler@gmail.com>
2017-04-20 16:52:18 +0100

    Change database for epjs #453

--- config/prod.exs
+++ config/prod.exs
@@ -27,7 +27,7 @@ config :healthlocker, Healthlocker.Repo,
 
 config :healthlocker, Healthlocker.ReadOnlyRepo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: System.get_env("EPJS_DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
