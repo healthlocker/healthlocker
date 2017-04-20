e2d4f0494436b702105cfc3d7d19221c52003616
katbow <katherine.bowler@gmail.com>
2017-04-20 16:35:29 +0100

    Fix test for new capitalisation #455 #458

--- test/models/epjs_user_test.exs
+++ test/models/epjs_user_test.exs
@@ -6,17 +6,17 @@ defmodule Healthlocker.EPJSUserTest do
   {:ok, start} = DateTime.from_naive(~N[1997-01-14 13:26:08.003], "Etc/UTC")
 
   @valid_attrs %{
-    patient_id: 202,
-    surname: "Burns",
-    forename: "Evan",
-    title: "Mr.",
-    patient_name: "Evan Burns",
-    trust_id: "uG0lDAx0S8",
-    nhs_number: "gLiyI9gsgoHjQc6pMcaT",
-    dob: dob,
-    spell_number: 3,
-    spell_start_date: start,
-    spell_end_date: nil
+    Patient_ID: 202,
+    Surname: "Burns",
+    Forename: "Evan",
+    Title: "Mr.",
+    Patient_Name: "Evan Burns",
+    Trust_ID: "uG0lDAx0S8",
+    NHS_Number: "gLiyI9gsgoHjQc6pMcaT",
+    DOB: dob,
+    Spell_Number: 3,
+    Spell_Start_Date: start,
+    Spell_End_Date: nil
   }
 
   @invalid_attrs %{}
