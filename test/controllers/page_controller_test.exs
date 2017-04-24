7442e2a9b7d7eef5f4c79d139bf833d4d3decf72
Cleop <cleo.pearson@hotmail.co.uk>
2017-04-24 12:33:02 +0100

    Removes test for tips on home page as the section no longer exists.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -5,7 +5,6 @@ defmodule Healthlocker.PageControllerTest do
   test "gets page when there are no stories or tips", %{conn: conn} do
     conn = get conn, page_path(conn, :index)
     assert html_response(conn, 200) =~ "No stories found"
-    assert html_response(conn, 200) =~ "No tips found"
   end
 
   test "GET /", %{conn: conn} do
