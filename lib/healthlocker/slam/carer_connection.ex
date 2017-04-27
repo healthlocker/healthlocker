6d4d6c2a552e5158adb4b3e8dfd31c1864a31eff
Tony Daly <tony.daly@me.com>
2017-04-27 11:55:33 +0200

    Add the epjs user id to the changeset.

--- lib/healthlocker/slam/carer_connection.ex
+++ lib/healthlocker/slam/carer_connection.ex
@@ -9,35 +9,41 @@ defmodule Healthlocker.Slam.CarerConnection do
     field :last_name
     field :date_of_birth
     field :nhs_number # should this be a string or int? Any other particulars?
+    field :epjs_user_id, :integer
   end
 
-  def changeset(connection, params \\ %{}) do
-    connection
-    |> cast(params, [:first_name, :last_name, :date_of_birth, :nhs_number])
+  def changeset(changeset, params \\ %{}) do
+    changeset
+    |> cast(params, [:first_name, :last_name, :date_of_birth, :nhs_number, :epjs_user_id])
     |> cast_datetime(:date_of_birth)
     |> validate_required([:first_name, :last_name, :date_of_birth, :nhs_number])
     |> validate_slam()
   end
 
   defp validate_slam(changeset, options \\ []) do
-    validate_change(changeset, :nhs_number, fn _, _ ->
-      %{changes: changes} = changeset
-      first_name = Map.get(changes, :first_name)
-      last_name = Map.get(changes, :last_name)
-      date_of_birth = Map.get(changes, :date_of_birth)
-      nhs_number = Map.get(changes, :nhs_number)
+    epjs_user = find_epjs_user(changeset)
 
-      query = from e in EPJSUser,
-        where: e."Forename" == ^first_name
-          and e."Surname" == ^last_name
-          and e."DOB" == type(^date_of_birth, :utc_datetime)
-          and e."NHS_Number" == ^nhs_number
-
-      valid? = Healthlocker.ReadOnlyRepo.one(query)
-
-      EPJSUser |> ReadOnlyRepo.all
-      if valid?, do: [], else: [{:nhs_number, options[:message] || "details do not match"}]
+    changeset = validate_change(changeset, :nhs_number, fn _, _ ->
+      if epjs_user, do: [], else: [{:nhs_number, options[:message] || "details do not match"}]
     end)
+
+    if epjs_user, do: put_change(changeset, :epjs_user_id, epjs_user.id), else: changeset
+  end
+
+  defp find_epjs_user(%{changes: changes}) when changes == %{} do
+    nil
+  end
+
+  defp find_epjs_user(%{changes: changes}) do
+    %{first_name: first_name, last_name: last_name, date_of_birth: date_of_birth, nhs_number: nhs_number} = changes
+
+    query = from e in EPJSUser,
+      where: e."Forename" == ^first_name
+        and e."Surname" == ^last_name
+        and e."DOB" == type(^date_of_birth, :utc_datetime)
+        and e."NHS_Number" == ^nhs_number
+
+    ReadOnlyRepo.one(query)
   end
 
   defp cast_datetime(changeset, field) do
