e2c99ec08c5a93d96d1098389663d2ac5a420df8
Tony Daly <tony.daly@me.com>
2017-04-27 16:08:51 +0200

    Refactor service user fetch.

--- web/controllers/care_team_controller.ex
+++ web/controllers/care_team_controller.ex
@@ -2,6 +2,15 @@ defmodule Healthlocker.CareTeamController do
   use Healthlocker.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> assign(:service_user, service_user_for(conn.assigns.current_user))
+    |> render("index.html")
+  end
+
+
+  defp service_user_for(carer) do
+    carer = carer |> Repo.preload(:caring)
+    [service_user | _] = carer.caring
+    service_user
   end
 end
--- web/views/care_team_view.ex
+++ web/views/care_team_view.ex
@@ -1,10 +1,4 @@
 defmodule Healthlocker.CareTeamView do
   use Healthlocker.Web, :view
   alias Healthlocker.Repo
-
-  def service_user_for(carer) do
-    carer = carer |> Repo.preload(:caring)
-    [service_user | _] = carer.caring
-    service_user
-  end
 end
