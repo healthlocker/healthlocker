b1ac5a52f41f599eb12d61133532ec42d8c0942d
Tony Daly <tony.daly@me.com>
2017-05-05 13:17:53 +0100

    Feature specs for carer sending messages.

---
+++ test/features/carer_message_test.exs
@@ -0,0 +1,53 @@
+defmodule Healthlocker.CarerMessageTest do
+  use Healthlocker.FeatureCase
+
+  setup %{session: session} do
+    carer = EctoFactory.insert(:user,
+      email: "tony@dwyl.io",
+      password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
+      terms_conditions: true,
+      privacy: true,
+      data_access: true
+    )
+
+    service_user = EctoFactory.insert(:user,
+      email: "bob@dwyl.io",
+      password_hash: Comeonin.Bcrypt.hashpwsalt("password"),
+      terms_conditions: true,
+      privacy: true,
+      data_access: true
+    )
+
+    Repo.insert!(%Healthlocker.Carer{carer: carer, caring: service_user})
+
+    Mix.Tasks.Healthlocker.Room.Create.run("run")
+
+    {:ok, %{session: session}}
+  end
+
+  @nav_button Query.css("#open-nav")
+  test "view care team messages", %{session: session} do
+    # As a user, who has clicked on Care team in the burger menu, I can write and send a message to that care team. Max characters 550.
+    session
+    |> log_in
+    |> click(@nav_button)
+    |> click(Query.link("Care team"))
+
+    assert current_path(session) == "/care-team/messages"
+  end
+
+  @message_field  Query.css("#message-input")
+  @messages_list  Query.css("#messages")
+  test "send care team message", %{session: session} do
+    session
+    |> log_in
+    |> click(@nav_button)
+    |> click(Query.link("Care team"))
+    |> fill_in(@message_field, with: "Hello there")
+    |> send_keys([:enter])
+    |> find(@messages_list, fn(messages) ->
+      messages
+      |> has_text?("Hello there")
+    end)
+  end
+end
