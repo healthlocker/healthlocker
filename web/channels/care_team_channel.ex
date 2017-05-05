d94946612eaf3e875de390cb6019350b6f4b5668
Tony Daly <tony.daly@me.com>
2017-05-05 16:52:37 +0100

    Tidy up unused files.

--- web/channels/care_team_channel.ex
+++
@@ -1,7 +0,0 @@
-defmodule Healthlocker.CareTeamChannel do
-  use Healthlocker.Web, :channel
-
-  def join("care-team:" <> user_id, _params, socket) do
-    {:ok, assign(socket, :user_id, String.to_integer(user_id)) }
-  end
-end
