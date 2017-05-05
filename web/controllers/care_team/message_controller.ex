8675b70e99097b093e412314e05048fadedcf803
Tony Daly <tony.daly@me.com>
2017-05-05 13:17:35 +0100

    404 if the Room doesn't exist.

--- web/controllers/care_team/message_controller.ex
+++ web/controllers/care_team/message_controller.ex
@@ -5,7 +5,7 @@ defmodule Healthlocker.CareTeam.MessageController do
   def show(conn, _params) do
     current_user = conn.assigns.current_user
     name = "carer-care-team:" <> Integer.to_string(current_user.id)
-    room = Repo.get_by(Room, name: name) |> Repo.preload([:users])
+    room = Repo.get_by!(Room, name: name) |> Repo.preload([:users])
 
     conn
     |> assign(:room, room)
