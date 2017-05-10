76fa5cc849803531bf785ecd10b8cf3e45ce4f2d
Tony Daly <tony.daly@me.com>
2017-05-10 17:20:01 +0100

    Default section is details.

--- web/controllers/caseload/user_controller.ex
+++ web/controllers/caseload/user_controller.ex
@@ -20,18 +20,6 @@ defmodule Healthlocker.Caseload.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-    user = Repo.get!(User, id)
-    slam_user = ReadOnlyRepo.one(from e in EPJSUser,
-                where: e."Patient_ID" == ^user.slam_id)
-    address = ReadOnlyRepo.one(from e in EPJSPatientAddressDetails,
-                    where: e."Patient_ID" == ^user.slam_id)
-    goals = Goal
-          |> Goal.get_goals(id)
-          |> Repo.all
-    strategies = Post
-                |> Post.get_coping_strategies(id)
-                |> Repo.all
-    render(conn, "details.html", user: user, slam_user: slam_user,
-          address: address, goals: goals, strategies: strategies)
+    show(conn, %{"id" => id, "section" => "details"})
   end
 end
