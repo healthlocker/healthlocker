89b9bb07121d5b559e434ba6377bd9b4f3bce87f
katbow <katherine.bowler@gmail.com>
2017-05-10 16:57:02 +0100

    Update users first and last name when connecting carer #600

--- web/controllers/slam_controller.ex
+++ web/controllers/slam_controller.ex
@@ -9,9 +9,17 @@ defmodule Healthlocker.SlamController do
   end
 
   def create(conn, %{"carer_connection" => params}) do
-    update_params =  Map.update!(params, "nhs_number", &(String.split(&1, " ")
-                                                          |> List.to_string))
+    update_params = if params["nhs_number"] do
+      params
+      |> Map.update!("nhs_number", &(String.split(&1, " ")
+      |> List.to_string))
+    else
+      params
+    end
+
     changeset = CarerConnection.changeset(%CarerConnection{}, update_params)
+
+    name_changeset = User.name_changeset(conn.assigns.current_user, %{first_name: update_params["first_name"], last_name: update_params["last_name"]})
 
     if changeset.valid? do
       slam_id = Ecto.Changeset.get_field(changeset, :epjs_patient_id)
@@ -20,20 +28,17 @@ defmodule Healthlocker.SlamController do
       service_user = Repo.one(query)
 
       if service_user do
-        case Repo.insert(%Carer{carer: conn.assigns.current_user, caring: service_user}) do
-          {:ok, _carer} ->
-            conn
-            |> put_flash(:info, "Account connected with SLaM")
-            |> redirect(to: account_path(conn, :index))
+        with {:ok, _user} <- Repo.update(name_changeset),
+             {:ok, _carer} <- Repo.insert(%Carer{carer: conn.assigns.current_user, caring: service_user}) do
+               conn
+               |> put_flash(:info, "Account connected with SLaM")
+               |> redirect(to: account_path(conn, :index))
+        else
           {:error, changeset} ->
             conn
             |> put_flash(:error, "Something went wrong")
             |> render("new.html", changeset: changeset)
-        end
-      else
-        conn
-        |> put_flash(:error, "Something went wrong")
-        |> render("new.html", changeset: changeset)
+       end
       end
     else
       conn
