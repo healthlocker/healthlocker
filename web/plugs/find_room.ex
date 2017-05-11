ee8175f11ff243a3baccd7bbde6b10463ba02544
Tony Daly <tony.daly@me.com>
2017-05-11 15:07:01 +0100

    Plug to find a room for a user
    We link to a user's messages in the sidebar. Therefore, on all pages
    we need to know the user's room so we can correctly link to it.

---
+++ web/plugs/find_room.ex
@@ -0,0 +1,20 @@
+defmodule Healthlocker.Plugs.FindRoom do
+  @moduledoc """
+  A small plug to find the room for a service user.
+  """
+
+  import Plug.Conn
+  alias Healthlocker.Repo
+
+  def find_room(conn, _options) do
+    current_user = conn.assigns[:current_user]
+    
+    if not is_nil(current_user.slam_id) do
+      room = Repo.one! Ecto.assoc(current_user, :rooms)
+      conn
+      |> assign(:room, room)
+    else
+      conn
+    end
+  end
+end
