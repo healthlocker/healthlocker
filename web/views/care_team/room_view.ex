6841046ffcb96919b68b6348d32379c30892606f
Tony Daly <tony.daly@me.com>
2017-05-11 15:11:25 +0100

    New care team room path.

--- web/views/care_team/room_view.ex
+++ web/views/care_team/room_view.ex
@@ -1,14 +1,3 @@
-defmodule Healthlocker.CareTeam.MessageView do
+defmodule Healthlocker.CareTeam.RoomView do
   use Healthlocker.Web, :view
-
-  def render("message.json", %{message: message}) do
-    %{
-      id: message.id,
-      body: message.body,
-      inserted_at: message.inserted_at,
-      user: %{
-        name: message.user.first_name <> " " <> message.user.last_name
-      }
-    }
-  end
 end
