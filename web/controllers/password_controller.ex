580d7f54ae6cd4a31b0d5ee40d76bfab70b12117
katbow <katherine.bowler@gmail.com>
2017-05-12 13:05:25 +0100

    Setup create action #176

--- web/controllers/password_controller.ex
+++ web/controllers/password_controller.ex
@@ -1,7 +1,32 @@
 defmodule Healthlocker.PasswordController do
   use Healthlocker.Web, :controller
 
+  alias Healthlocker.User
+
   def new(conn, _params) do
-    render conn, "new.html"
+    changeset = User.email_changeset(%User{})
+    render conn, "new.html", changeset: changeset, action: password_path(conn, :create)
+  end
+
+  def create(conn, %{"user" => pw_params}) do
+    user = case pw_params["email"] do
+      nil ->
+        nil
+      email ->
+        User
+        |> Repo.get_by(email: pw_params["email"])
+    end
+    
+    case user do
+      nil ->
+        conn
+        |> put_flash(:error, "Could not send reset email. Please try again later")
+        |> redirect(to: login_path(conn, :index))
+      user ->
+        # generate token here + sent at and save
+        conn
+        |> put_flash(:info, "Password reset sent")
+        |> redirect(to: login_path(conn, :index))
+    end
   end
 end
