01e515ebdc300b6c10326dad7694ce653f4b261b
katbow <katherine.bowler@gmail.com>
2017-05-12 14:41:05 +0100

    Generate token and sent at date when a valid email is entered #176

--- web/controllers/password_controller.ex
+++ web/controllers/password_controller.ex
@@ -9,24 +9,42 @@ defmodule Healthlocker.PasswordController do
   end
 
   def create(conn, %{"user" => pw_params}) do
-    user = case pw_params["email"] do
+    email = pw_params["email"]
+    user = case email do
       nil ->
         nil
       email ->
         User
-        |> Repo.get_by(email: pw_params["email"])
+        |> Repo.get_by(email: email)
     end
-    
+
     case user do
       nil ->
         conn
         |> put_flash(:error, "Could not send reset email. Please try again later")
         |> redirect(to: login_path(conn, :index))
       user ->
-        # generate token here + sent at and save
+        reset_password_token(user)
+        # send password token to pw_params["email"]
         conn
         |> put_flash(:info, "Password reset sent")
         |> redirect(to: login_path(conn, :index))
     end
+  end
+
+  defp reset_password_token(user) do
+    token = random_string(48)
+    sent_at = DateTime.utc_now
+
+    user
+    |> User.password_token_changeset(%{reset_password_token: token, reset_token_sent_at: sent_at})
+    |> Repo.update!
+  end
+
+  defp random_string(length) do
+    length
+    |> :crypto.strong_rand_bytes
+    |> Base.url_encode64
+    |> binary_part(0, length)
   end
 end
