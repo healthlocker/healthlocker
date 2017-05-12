905648122794595b5687b5f6bdd12a40aa0c5b51
katbow <katherine.bowler@gmail.com>
2017-05-12 15:42:37 +0100

    Create send reset email function #176

--- lib/email.ex
+++ lib/email.ex
@@ -1,4 +1,4 @@
-defmodule Healthlocker.Feedback do
+defmodule Healthlocker.Email do
   use Bamboo.Phoenix, view: Healthlocker.FeedbackView
 
   def send_feedback(subject, message) do
@@ -7,5 +7,13 @@ defmodule Healthlocker.Feedback do
     |> from(System.get_env("FROM_EMAIL"))
     |> subject(subject)
     |> text_body(message)
+  end
+
+  def send_reset_email(to_email, token) do
+    new_email()
+    |> to(to_email)
+    |> from("healthlocker.test@gmail.com")
+    |> subject("Healthlocker Reset Password Instructions")
+    |> text_body("Please visit https://www.healthlocker.uk/password/#{token} to reset your password")
   end
 end
