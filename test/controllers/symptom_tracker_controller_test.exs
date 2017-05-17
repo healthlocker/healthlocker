719bafdeee73aac2712567dd024112022336c240
robstallion <robertefrancis18@gmail.com>
2017-05-17 18:13:11 +0100

    Test new action in symptom tracker controller #560

---
+++ test/controllers/symptom_tracker_controller_test.exs
@@ -0,0 +1,23 @@
+defmodule Healthlocker.SymptomTrackerControllerTest do
+  use Healthlocker.ConnCase
+  alias Healthlocker.User
+
+  describe "Current user is assigned to the conn" do
+    setup do
+      %User{
+        id: 123456,
+        first_name: "My",
+        last_name: "Name",
+        email: "abc@gmail.com",
+        password_hash: Comeonin.Bcrypt.hashpwsalt("password")
+      } |> Repo.insert
+
+      {:ok, conn: build_conn() |> assign(:current_user, Repo.get(User, 123456)) }
+    end
+
+    test "GET /symptom-tracker/new", %{conn: conn} do
+      conn = get conn, symptom_tracker_path(conn, :new)
+      assert html_response(conn, 200) =~ "Symptom"
+    end
+  end
+end
