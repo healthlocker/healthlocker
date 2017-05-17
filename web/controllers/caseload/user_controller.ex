0b42c4bcef78c2990e58f71f7f693375ffbf887a
robstallion <robertefrancis18@gmail.com>
2017-05-17 13:26:25 +0100

    Add sleep_data and date to be for viewing sleep tracker #612

--- web/controllers/caseload/user_controller.ex
+++ web/controllers/caseload/user_controller.ex
@@ -1,9 +1,11 @@
 defmodule Healthlocker.Caseload.UserController do
   use Healthlocker.Web, :controller
+  alias Healthlocker.SleepTracker
 
   alias Healthlocker.{User, ReadOnlyRepo, EPJSUser, EPJSPatientAddressDetails, Goal, Post, Slam.ServiceUser}
 
   def show(conn, %{"id" => id, "section" => section}) do
+    date = Date.to_iso8601(Date.utc_today())
     user = Repo.get!(User, id)
     room = Repo.one! assoc(user, :rooms)
     service_user = ServiceUser.for(user)
@@ -17,8 +19,13 @@ defmodule Healthlocker.Caseload.UserController do
     strategies = Post
                 |> Post.get_coping_strategies(id)
                 |> Repo.all
+    sleep_data = SleepTracker
+      |> SleepTracker.get_sleep_data(service_user.id, Date.utc_today())
+      |> Repo.all
+
     render(conn, String.to_atom(section), user: service_user, slam_user: slam_user,
-          address: address, goals: goals, strategies: strategies, room: room)
+          address: address, goals: goals, strategies: strategies, room: room,
+          sleep_data: sleep_data, date: date)
   end
 
   def show(conn, %{"id" => id}) do
