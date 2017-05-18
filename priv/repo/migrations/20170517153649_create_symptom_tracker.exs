651056e22516bff7a8ba7703e392c76649f23a65
robstallion <robertefrancis18@gmail.com>
2017-05-18 17:09:30 +0100

    Rename table in db for clarity

--- priv/repo/migrations/20170517153649_create_symptom_tracker.exs
+++ priv/repo/migrations/20170517153649_create_symptom_tracker.exs
@@ -2,7 +2,7 @@ defmodule Healthlocker.Repo.Migrations.CreateSymptomTracker do
   use Ecto.Migration
 
   def change do
-    create table(:symptom_tracker) do
+    create table(:symptom_trackers) do
       add :affected_scale, :string
       add :notes, :string
       add :symptom_id, references(:symptoms, on_delete: :delete_all)
