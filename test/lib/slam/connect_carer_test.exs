b00c3da7a3ac23e90ca161111dd81b0643bc617b
katbow <katherine.bowler@gmail.com>
2017-05-18 19:00:07 +0100

    Test multi #667

--- test/lib/slam/connect_carer_test.exs
+++ test/lib/slam/connect_carer_test.exs
@@ -26,8 +26,25 @@ defmodule Healthlocker.Slam.ConnectCarerTest do
   test "dry carer connection run" do
     user = Repo.get!(User, 123456)
     service_user = Repo.get!(User, 123457)
-    multi = ConnectCarer.connect_carer_and_create_rooms(user, %{"first_name" => "Kat", "last_name" => "Bow"}, service_user)
-    assert [user_name: {:update, user_changeset, []},
-  carer: {:insert, carer_changeset, []}] = Ecto.Multi.to_list(multi)
+    multi = ConnectCarer.connect_carer_and_create_rooms(user, %{
+      "first_name" => "Kat",
+      "last_name" => "Bow"
+    }, service_user)
+    #
+    assert [user: {:update, user_changeset, []},
+            carer: {:insert, carer_changeset, []}, room: {:run, function},
+            carer_room: {:run, function},
+            clinician_room: {:run, function}] = Ecto.Multi.to_list(multi)
+    [user, carer, room, carer_room, clinician_room] = Ecto.Multi.to_list(multi)
+    IO.inspect room
+    IO.inspect carer_room
+    assert [user, carer, room, carer_room, clinician_room] = Ecto.Multi.to_list(multi)
   end
 end
+# [user: {:update, changeset, []},
+# carer: {:insert, changeset, []},
+# room: {:run, function,
+#              carer_room: {:run,
+#               #Function<1.84387637/1 in Healthlocker.Slam.ConnectCarer.connect_carer_and_create_rooms/3>},
+#              clinician_room: {:run,
+#               #Function<2.84387637/1 in Healthlocker.Slam.ConnectCarer.connect_carer_and_create_rooms/3>}]
