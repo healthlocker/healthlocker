1aaf41bd2973d7c06ca78c1136535c721e4e8f3f
robstallion <robertefrancis18@gmail.com>
2017-05-18 19:14:27 +0100

    Update action in symptom tracker. #560

--- web/controllers/symptom_tracker_controller.ex
+++ web/controllers/symptom_tracker_controller.ex
@@ -4,10 +4,25 @@ defmodule Healthlocker.SymptomTrackerController do
 
   def new(conn, _params) do
     user_id = conn.assigns.current_user.id
-    query = from s in Symptom, where: s.user_id == ^user_id, select: s.symptom
-    [symptom | _] = Repo.all(query)
-    changeset = SymptomTracker.changeset(%SymptomTracker{}, %{})
-    render conn, "new.html", changeset: changeset, symptom: symptom
+    query = from s in Symptom, where: s.user_id == ^user_id
+
+    case Repo.all(query) do
+      [symptom | _] ->
+        tracker_query = from st in SymptomTracker, where: st.symptom_id == ^symptom.id, order_by: [st.inserted_at]
+        [symptom_tracker | _] = Repo.all(tracker_query)
+        case Date.compare(NaiveDateTime.to_date(symptom_tracker.inserted_at), Date.utc_today) do
+          :eq ->
+            conn
+            |> put_flash(:error, "You can only track your problem once a day")
+            |> redirect(to: toolkit_path(conn, :index))
+          _ ->
+            changeset = SymptomTracker.changeset(%SymptomTracker{}, %{})
+            render conn, "new.html", changeset: changeset, symptom: symptom
+        end
+      [] ->
+        conn
+        |> redirect(to: symptom_path(conn, :new))
+    end
   end
 
   def create(conn, %{"symptom_tracker" => symptom_tracker}) do
