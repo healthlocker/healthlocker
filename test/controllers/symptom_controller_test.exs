7779ce4be9e501281895603d24a60b2f5805764e
robstallion <robertefrancis18@gmail.com>
2017-05-19 14:47:15 +0100

    Create tests for symptom controller #560

--- test/controllers/symptom_controller_test.exs
+++ test/controllers/symptom_controller_test.exs
@@ -1,6 +1,9 @@
 defmodule Healthlocker.SymptomControllerTest do
   use Healthlocker.ConnCase
-  alias Healthlocker.User
+  alias Healthlocker.{User, Symptom}
+
+  @valid_attrs %{symptom: "a problem"}
+  @invalid_attrs %{symptom: ""}
 
   describe "Current user is assigned to the conn" do
     setup do
@@ -17,7 +20,17 @@ defmodule Healthlocker.SymptomControllerTest do
 
     test "GET /symptom/new", %{conn: conn} do
       conn = get conn, symptom_path(conn, :new)
-      assert html_response(conn, 200) =~ "Symptom"
+      assert html_response(conn, 200) =~ "Problem tracker"
+    end
+
+    test "POST /symptom", %{conn: conn} do
+      conn = post conn, symptom_path(conn, :create), symptom: @valid_attrs
+      assert redirected_to(conn) == symptom_tracker_path(conn, :new)
+    end
+
+    test "does not create symptom and renders errors when data is invalid", %{conn: conn} do
+      conn = post conn, symptom_path(conn, :create), symptom: @invalid_attrs
+      assert html_response(conn, 200) =~ "Problem tracker"
     end
   end
 end
