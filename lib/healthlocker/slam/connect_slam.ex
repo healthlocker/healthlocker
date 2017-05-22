ac6ba8ef2598a1aaccdd78c205d19f4b0355bcd0
katbow <katherine.bowler@gmail.com>
2017-05-22 10:44:03 +0100

    Create & run function to add clinicians to room #666

--- lib/healthlocker/slam/connect_slam.ex
+++ lib/healthlocker/slam/connect_slam.ex
@@ -1,6 +1,6 @@
 defmodule Healthlocker.Slam.ConnectSlam do
   alias Ecto.Multi
-  alias Healthlocker.{User, Room, UserRoom, Repo}
+  alias Healthlocker.{User, Room, UserRoom, Repo, ClinicianRooms}
 
   def connect_su_and_create_rooms(user, params) do
     Multi.new
@@ -9,6 +9,7 @@ defmodule Healthlocker.Slam.ConnectSlam do
       name: "service-user-care-team:" <> Integer.to_string(user.id)
     }))
     |> Multi.run(:user_room, &add_su_to_room/1)
+    |> Multi.run(:clinician_room, &add_clinicians_to_room/1)
   end
 
   defp add_su_to_room(multi) do
@@ -22,5 +23,29 @@ defmodule Healthlocker.Slam.ConnectSlam do
       {:error, changeset} ->
         {:error, changeset, "Error adding user to room"}
     end
+  end
+
+  defp add_clinicians_to_room(multi) do
+    care_team = Healthlocker.Slam.CareTeam.for(multi.user)
+    clinicians = make_clinicians(care_team, multi.room.id)
+
+    case Repo.insert_all(ClinicianRooms, clinicians) do
+      {n, nil} ->
+        {:ok, n}
+      err ->
+        {:error, "Error adding clinician to room"}
+    end
+  end
+
+  defp make_clinicians(care_team, room_id) do
+    care_team
+    |> Enum.map(fn(clinician) ->
+      %{
+        room_id: room_id,
+        clinician_id: clinician.id,
+        inserted_at: DateTime.utc_now(),
+        updated_at: DateTime.utc_now()
+      }
+    end)
   end
 end
