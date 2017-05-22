f45b69122cf102b5bda99e43d67b3e99fbf9c01c
katbow <katherine.bowler@gmail.com>
2017-05-22 10:19:19 +0100

    Create and run function to add user_rooms to multi #666

--- lib/healthlocker/slam/connect_slam.ex
+++ lib/healthlocker/slam/connect_slam.ex
@@ -1,10 +1,26 @@
 defmodule Healthlocker.Slam.ConnectSlam do
   alias Ecto.Multi
-  alias Healthlocker.{User, Room}
+  alias Healthlocker.{User, Room, UserRoom, Repo}
 
   def connect_su_and_create_rooms(user, params) do
     Multi.new
     |> Multi.update(:user, User.connect_slam(user, params))
-    |> Multi.insert(:room, Room.changeset(%Room{}, %{name: "service-user-care-team:" <> Integer.to_string(user.id)}))
+    |> Multi.insert(:room, Room.changeset(%Room{}, %{
+      name: "service-user-care-team:" <> Integer.to_string(user.id)
+    }))
+    |> Multi.run(:user_room, &add_su_to_room/1)
+  end
+
+  defp add_su_to_room(multi) do
+    changeset = UserRoom.changeset(%UserRoom{}, %{
+      user_id: multi.user.id,
+      room_id: multi.room.id
+    })
+    case Repo.insert(changeset) do
+      {:ok, user_room} ->
+        {:ok, user_room}
+      {:error, changeset} ->
+        {:error, changeset, "Error adding user to room"}
+    end
   end
 end
