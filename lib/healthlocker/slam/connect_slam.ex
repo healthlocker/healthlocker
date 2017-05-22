b98b033f0cc5beb82606fdbcbdd8e67160b92589
katbow <katherine.bowler@gmail.com>
2017-05-22 09:50:56 +0100

    Insert room when connecting a slam user #666

--- lib/healthlocker/slam/connect_slam.ex
+++ lib/healthlocker/slam/connect_slam.ex
@@ -1,9 +1,10 @@
 defmodule Healthlocker.Slam.ConnectSlam do
   alias Ecto.Multi
-  alias Healthlocker.{User}
+  alias Healthlocker.{User, Room}
 
   def connect_su_and_create_rooms(user, params) do
     Multi.new
     |> Multi.update(:user, User.connect_slam(user, params))
+    |> Multi.insert(:room, Room.changeset(%Room{}, %{name: "service-user-care-team:" <> Integer.to_string(user.id)}))
   end
 end
