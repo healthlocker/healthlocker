de09e3fcb9f2e1ed220167c9a219e5d1a7b47661
katbow <katherine.bowler@gmail.com>
2017-05-23 13:59:59 +0100

    Create and run function to delete user_room #685

--- lib/healthlocker/slam/disconnect_slam.ex
+++ lib/healthlocker/slam/disconnect_slam.ex
@@ -1,9 +1,20 @@
 defmodule Healthlocker.Slam.DisconnectSlam do
   alias Ecto.Multi
-  alias Healthlocker.{User}
+  alias Healthlocker.{User, Repo, UserRoom}
 
   def disconnect_su(user) do
     Multi.new
     |> Multi.update(:user, User.disconnect_changeset(user))
+    |> Multi.run(:user_room, &delete_user_room/1)
+  end
+
+  def delete_user_room(multi) do
+    user_room = Repo.get_by!(UserRoom, user_id: multi.user.id)
+    case Repo.delete(user_room) do
+      {:ok, user_room} ->
+        {:ok, user_room}
+      {:error, changeset} ->
+        {:error, changeset, "Error deleting user_room"}
+    end
   end
 end
