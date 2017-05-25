0c0da5500bd5f70040a517a075dfa448a6501499
robstallion <robertefrancis18@gmail.com>
2017-05-25 12:49:57 +0100

    Create a function to view markdown as html. #680

--- web/views/caseload/user_view.ex
+++ web/views/caseload/user_view.ex
@@ -5,4 +5,11 @@ defmodule Healthlocker.Caseload.UserView do
     String.slice(nhs_number, 0, 3) <> " " <> String.slice(nhs_number, 3, 3) <> " " <>
       String.slice(nhs_number, 6, 4)
   end
+
+  def markdown(body) do
+    body
+    |> Earmark.as_html!
+    |> raw
+  end
+
 end
