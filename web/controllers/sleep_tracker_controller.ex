48d99c762b0ff84c2fee4948b6b3c4ee60aade3a
katbow <katherine.bowler@gmail.com>
2017-06-09 17:47:26 +0100

    Move logic to only enter sleep once to new #816

--- web/controllers/sleep_tracker_controller.ex
+++ web/controllers/sleep_tracker_controller.ex
@@ -4,8 +4,20 @@ defmodule Healthlocker.SleepTrackerController do
   alias Healthlocker.{SleepTracker}
 
   def new(conn, _params) do
-    changeset = SleepTracker.changeset(%SleepTracker{})
-    render(conn, "new.html", changeset: changeset)
+    user = conn.assigns.current_user
+    sleep_data = SleepTracker
+                |> SleepTracker.get_sleep_data_today(user.id)
+                |> Repo.one
+
+    case sleep_data do
+      nil ->
+        changeset = SleepTracker.changeset(%SleepTracker{})
+        render(conn, "new.html", changeset: changeset)
+      _ ->
+        conn
+        |> put_flash(:error, "You can only enter sleep once per day.")
+        |> redirect(to: toolkit_path(conn, :index))
+    end
   end
 
   def create(conn, %{"sleep_tracker" => st_params}) do
@@ -19,20 +31,14 @@ defmodule Healthlocker.SleepTrackerController do
                 |> SleepTracker.get_sleep_data_today(user.id)
                 |> Repo.one
 
-    if sleep_data do
-      conn
-      |> put_flash(:error, "You can only enter sleep once per day.")
-      |> redirect(to: toolkit_path(conn, :index))
-    else
-      case Repo.insert(changeset) do
-        {:ok, params} ->
-          conn |> track_created(params)
-          conn
-          |> put_flash(:info, "Sleep tracked successfully!")
-          |> redirect(to: toolkit_path(conn, :index))
-        {:error, changeset} ->
-          render(conn, "new.html", changeset: changeset)
-      end
+    case Repo.insert(changeset) do
+      {:ok, params} ->
+        conn |> track_created(params)
+        conn
+        |> put_flash(:info, "Sleep tracked successfully!")
+        |> redirect(to: toolkit_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
