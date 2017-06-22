ce204bf25dbb56b8b3a3f5b860369f3d23bf2076
robstallion <robertefrancis18@gmail.com>
2017-06-22 17:55:00 +0100

    update the readonlyrepo db config so that it can read from the azure db on the vm

--- config/dev.exs
+++ config/dev.exs
@@ -44,6 +44,9 @@ config :healthlocker, Healthlocker.Repo,
 
 config :healthlocker, Healthlocker.ReadOnlyRepo,
   adapter: MssqlEcto,
-  hostname: "localhost"
+  hostname: System.get_env("READ_ONLY_HOSTNAME"),
+  username: System.get_env("READ_ONLY_USERNAME"),
+  password: System.get_env("READ_ONLY_PASSWORD"),
+  database: System.get_env("READ_ONLY_DATABASE")
 
 config :healthlocker, :analytics, Healthlocker.Analytics.Segment
