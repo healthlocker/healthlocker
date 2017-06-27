0362e717b3ac7342db20254626234484060758f6
katbow <katherine.bowler@gmail.com>
2017-06-27 16:16:29 +0100

    Check is user attempting to access clinician room is a clinician #869 There was previously a check to see if the user was logged in, but no check to see they were the right kind of user.

--- web/controllers/caseload/room_controller.ex
+++ web/controllers/caseload/room_controller.ex
@@ -3,24 +3,30 @@ defmodule Healthlocker.Caseload.RoomController do
   use Healthlocker.Web, :controller
 
   def show(conn, %{"id" => id, "user_id" => user_id}) do
-    room = Repo.get((from r in Room, preload: [messages: :user]), id)
-    messages = Repo.all from m in Message,
+    if conn.assigns.current_user.user_guid do
+      room = Repo.get((from r in Room, preload: [messages: :user]), id)
+      messages = Repo.all from m in Message,
       where: m.room_id == ^room.id,
       order_by: [asc: :inserted_at, asc: :id],
       preload: [:user]
 
-    user = Repo.get!(User, user_id)
-    service_user = ServiceUser.for(user)
-    slam_user = ReadOnlyRepo.one(from e in EPJSUser,
-                where: e."Patient_ID" == ^service_user.slam_id)
+      user = Repo.get!(User, user_id)
+      service_user = ServiceUser.for(user)
+      slam_user = ReadOnlyRepo.one(from e in EPJSUser,
+      where: e."Patient_ID" == ^service_user.slam_id)
 
-    conn
-    |> assign(:service_user, service_user)
-    |> assign(:room, room)
-    |> assign(:messages, messages)
-    |> assign(:slam_user, slam_user)
-    |> assign(:user, user)
-    |> assign(:current_user_id, conn.assigns.current_user.id)
-    |> render("show.html")
+      conn
+      |> assign(:service_user, service_user)
+      |> assign(:room, room)
+      |> assign(:messages, messages)
+      |> assign(:slam_user, slam_user)
+      |> assign(:user, user)
+      |> assign(:current_user_id, conn.assigns.current_user.id)
+      |> render("show.html")
+    else
+      conn
+      |> put_flash(:error, "Authentication failed")
+      |> redirect(to: page_path(conn, :index))
+    end
   end
 end
