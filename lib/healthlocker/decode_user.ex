b002bd31ee5690be8027786329764bd1b1c19215
robstallion <robertefrancis18@gmail.com>
2017-06-27 21:29:37 +0100

    create function to decode user data from ePJS. #868

---
+++ lib/healthlocker/decode_user.ex
@@ -0,0 +1,20 @@
+defmodule Healthlocker.DecodeUser do
+  alias Healthlocker.ReadOnlyRepo
+
+  def decode_user_data(user_str) do
+    pass = System.get_env("MPass")
+    query = """
+      DECLARE @UserData VARBINARY(MAX), @passphrase varchar(255), @UserDataString VARCHAR(MAX);
+      SET @passphrase = HASHBYTES('MD5', N'#{pass}');
+      SELECT @UserData =
+              CAST(N'' AS XML).value(
+                  'xs:base64Binary("#{user_str}")'
+                , 'VARBINARY(MAX)'
+              )
+      ;
+      SELECT CONVERT(VARCHAR(8000),DecryptByPassPhrase(@passphrase,@UserData));
+    """
+
+    Ecto.Adapters.SQL.query(ReadOnlyRepo, query, [])
+  end
+end
