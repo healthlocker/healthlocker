b3eba4425ea851c678bdc83e94740167cd66ae01
katbow <katherine.bowler@gmail.com>
2017-06-28 16:21:22 +0100

    Set default value for testing #868

--- lib/healthlocker/decrypt_user.ex
+++ lib/healthlocker/decrypt_user.ex
@@ -2,31 +2,36 @@ defmodule Healthlocker.DecryptUser do
   alias Healthlocker.ReadOnlyRepo
 
   def decrypt_user_data(user_str) do
-    pass = System.get_env("MPass")
-    query = """
-      DECLARE @UserData VARBINARY(MAX), @passphrase varchar(255), @UserDataString VARCHAR(MAX);
-      SET @passphrase = HASHBYTES('MD5', N'#{pass}');
-      SELECT @UserData =
-              CAST(N'' AS XML).value(
-                  'xs:base64Binary("#{user_str}")'
-                , 'VARBINARY(MAX)'
-              )
-      ;
-      SELECT CONVERT(VARCHAR(8000),DecryptByPassPhrase(@passphrase,@UserData));
-    """
+    case Mix.env do
+      :test ->
+        "randomstringtotestwith"
+      _ ->
+        pass = System.get_env("MPass")
+        query = """
+          DECLARE @UserData VARBINARY(MAX), @passphrase varchar(255), @UserDataString VARCHAR(MAX);
+          SET @passphrase = HASHBYTES('MD5', N'#{pass}');
+          SELECT @UserData =
+                  CAST(N'' AS XML).value(
+                      'xs:base64Binary("#{user_str}")'
+                    , 'VARBINARY(MAX)'
+                  )
+          ;
+          SELECT CONVERT(VARCHAR(8000),DecryptByPassPhrase(@passphrase,@UserData));
+        """
 
-    case Ecto.Adapters.SQL.query(ReadOnlyRepo, query, []) do
-      {:ok, result} ->
-        result.rows
-        |> List.flatten
-        |> List.first
-        |> get_user_guid
-      {:error, _} ->
-        ""
+        case Ecto.Adapters.SQL.query(ReadOnlyRepo, query, []) do
+          {:ok, result} ->
+            result.rows
+            |> List.flatten
+            |> List.first
+            |> get_user_guid
+          {:error, _} ->
+            ""
+        end
     end
   end
 
-  defp get_user_guid(str) do
+  def get_user_guid(str) do
     case str do
       nil ->
         ""
