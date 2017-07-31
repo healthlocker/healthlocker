5fe8e744a109242ea7243f853a050b09f12ac960
robstallion <robertefrancis18@gmail.com>
2017-07-31 16:17:48 +0100

    make care team for query the epjs_app #941

--- lib/healthlocker/slam/care_team.ex
+++ lib/healthlocker/slam/care_team.ex
@@ -3,13 +3,10 @@ defmodule Healthlocker.Slam.CareTeam do
   import Ecto.Query
 
   @moduledoc """
-  For a given service user query EPJS for all the associated clinicians,
-  these will form the user's care team.
+  For a given service user make api call to epjs_app for all the associated
+  clinicians, these will form the user's care team.
   """
   def for(user) do
-    query = from e in EPJSTeamMember,
-      where: e."Patient_ID" == ^user.slam_id
-
-    ReadOnlyRepo.all(query)
+    CallEpjs.query_epjs("http://localhost:4001/care-team/for?service_user=", user)
   end
 end
