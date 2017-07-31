525ebee29e6e161e2f3714d9f718aed6eac8ed9c
katbow <katherine.bowler@gmail.com>
2017-07-31 18:30:59 +0100

    Refactor call to set app layout https://github.com/healthlocker/oxleas-adhd/issues/51

--- web/plugs/find_app_name.ex
+++ web/plugs/find_app_name.ex
@@ -4,26 +4,30 @@ defmodule Healthlocker.Plugs.AppName do
   def init(opts), do: opts
 
   def call(conn, _) do
-    if conn.assigns[:app_name] do
-      conn
-    else
-      conn
-      |> put_app_name(Healthlocker.Endpoint.url())
-    end
-  end
-
-  defp put_app_name(conn, current_url) do
-    # this will later default to "healthlocker", unless it is the url for oxleas
-    case current_url do
+    case Healthlocker.Endpoint.url() do
       "http://localhost:4000"->
         conn
-        |> Phoenix.Controller.put_layout({Healthlocker.HealthlockerLayoutView, "app.html"})
       "https://www.healthlocker.uk/" ->
         conn
-        |> Phoenix.Controller.put_layout({Healthlocker.HealthlockerLayoutView, "app.html"})
       _ ->
         conn
         |> Phoenix.Controller.put_layout({Healthlocker.OxleasLayoutView, "app.html"})
     end
   end
+
+  # Will use this for /entry point and call from above. Can set something in conn to indicate it's been set manually.
+  # defp put_app_name(conn, current_url) do
+  #   # this will later default to "healthlocker", unless it is the url for oxleas
+  #   case current_url do
+  #     "http://localhost:4000"->
+  #       conn
+  #       |> Phoenix.Controller.put_layout({Healthlocker.HealthlockerLayoutView, "app.html"})
+  #     "https://www.healthlocker.uk/" ->
+  #       conn
+  #       |> Phoenix.Controller.put_layout({Healthlocker.HealthlockerLayoutView, "app.html"})
+  #     _ ->
+  #       conn
+  #       |> Phoenix.Controller.put_layout({Healthlocker.OxleasLayoutView, "app.html"})
+  #   end
+  # end
 end
