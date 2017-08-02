77ccc479dcf2d2599d026f076368d57a84e03ee6
nelsonic <contact.nelsonic+github@gmail.com>
2017-08-02 14:03:12 +0100

    update version.sh script to match healthlocker_

--- .deliver/version.sh
+++ .deliver/version.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # echo $DIR
 FILENAME=$(ls -t "$DIR/releases" | head -n 1)
-NO_APP_NAME=$(echo $FILENAME | sed 's/hello_world_edeliver_//g')
+NO_APP_NAME=$(echo $FILENAME | sed 's/healthlocker_//g')
 VERSION=$(echo $NO_APP_NAME | sed 's/.upgrade.tar.gz//g')
 echo $VERSION
