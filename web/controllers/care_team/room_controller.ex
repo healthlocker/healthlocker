35fb3f2c94df9ed9c159d349d490b06af35301d7
robstallion <robertefrancis18@gmail.com>
2017-08-22 19:26:51 +0100

    update the way clinician rooms is updated

--- web/controllers/care_team/room_controller.ex
+++ web/controllers/care_team/room_controller.ex
@@ -5,7 +5,7 @@ defmodule Healthlocker.CareTeam.RoomController do
   def show(conn, %{"id" => id}) do
     room = Repo.get! assoc(conn.assigns.current_user, :rooms), id
     service_user = Healthlocker.Slam.ServiceUser.for(conn.assigns.current_user)
-    clinicians_list = Healthlocker.Slam.CareTeam.for(service_user)
+    care_team = Healthlocker.Slam.CareTeam.for(service_user)
     Repo.delete_all(from cr in ClinicianRooms, where: cr.room_id == ^id) # delete all from ClinicianRooms where id == room.id
 
     messages = Repo.all from m in Message,
@@ -13,15 +13,28 @@ defmodule Healthlocker.CareTeam.RoomController do
       order_by: [asc: :inserted_at, asc: :id],
       preload: [:user]
 
-    add_clinicians_to_clinician_rooms(clinicians_list, id)
-    return_conn(conn, room, service_user, messages)
+    clinicians = make_clinician_rooms_entries(care_team, id)
+
+    case Repo.insert_all(ClinicianRooms, clinicians) do
+      {n, nil} ->
+        return_conn(conn, room, service_user, messages)
+      _err ->
+        conn
+        |> redirect(to: page_path(conn, :index))
+    end
   end
 
-  def add_clinicians_to_clinician_rooms(clinician_list, room_id) do
-    clinician_list
-    |> Enum.each(fn clinician ->
-      Repo.insert!(%{clinician_id: clinician."Staff_ID", room_id: room_id})
+  def make_clinician_rooms_entries(care_team, room_id) do
+    care_team
+    |> Enum.map(fn(clinician) ->
+      %{
+        room_id: room_id,
+        clinician_id: clinician."Staff_ID",
+        inserted_at: DateTime.utc_now(),
+        updated_at: DateTime.utc_now()
+      }
     end)
+    |> Enum.uniq_by(fn %{clinician_id: x} -> x end)
   end
 
   def return_conn(conn, room, service_user, messages) do
@@ -30,7 +43,6 @@ defmodule Healthlocker.CareTeam.RoomController do
     |> assign(:service_user, service_user)
     |> assign(:messages, messages)
     |> assign(:current_user_id, conn.assigns.current_user.id)
-    |> Healthlocker.SetView.set_view("CareTeam.RoomView")
     |> render("show.html")
   end
 end
