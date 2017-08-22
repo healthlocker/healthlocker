cc905377a6ce63950b2153e582ca42921349a710
Cleop <cleo.pearson@hotmail.co.uk>
2017-08-22 11:16:39 +0100

    #767 Corrects date w/p st/th

--- web/views/tracker_view.ex
+++ web/views/tracker_view.ex
@@ -201,7 +201,7 @@ defmodule Healthlocker.TrackerView do
   end
 
   def date_with_day_and_month(date) do
-    day_of_week(date, 0) <> " " <> Integer.to_string(date.day) <> "th " <> Timex.month_name(date.month)
+    day_of_week(date, 0) <> " " <> Integer.to_string(date.day) <> Timex.month_name(date.month)
   end
 
   def printed_time(naive_date_time) do
