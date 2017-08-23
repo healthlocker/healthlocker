571ebf984f88ca4a4f2a88e4d35db4b0a0f97671
robstallion <robertefrancis18@gmail.com>
2017-08-23 17:41:33 +0100

    update carer model to allow for carers to connect to non hl users #762

--- web/models/carer.ex
+++ web/models/carer.ex
@@ -5,12 +5,13 @@ defmodule Healthlocker.Carer do
   schema "carers" do
     belongs_to :carer, Healthlocker.User
     belongs_to :caring, Healthlocker.User
+    field :slam_id, :integer
   end
 
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:carer_id, :caring_id])
-    |> validate_required([:carer_id, :caring_id])
-    |> unique_constraint(:carer_id, name: :carers_caring_id_carer_id_index)
+    |> cast(params, [:carer_id, :caring_id, :slam_id])
+    |> validate_required([:carer_id, :slam_id])
+    |> unique_constraint(:carer_id, name: :carers_carer_id_slam_id_index)
   end
 end
