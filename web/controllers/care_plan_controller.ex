07e85781cebe82e28e8cf3480b6ec669c17e61d8
Cleop <cleo.pearson@hotmail.co.uk>
2017-08-24 19:05:12 +0100

    #42 creates option of nil

--- web/controllers/care_plan_controller.ex
+++ web/controllers/care_plan_controller.ex
@@ -27,25 +27,34 @@ defmodule Healthlocker.CarePlanController do
                                where: esn."Patient_ID" == ^patient_id
     recovery_care_plan = ReadOnlyRepo.one(recovery_care_plan_query)
 
-    life_event_triggers_query = from esn in EPJSRcpLifeEventTriggers,
+    if recovery_care_plan do
+      life_event_triggers_query = from esn in EPJSRcpLifeEventTriggers,
       where: esn."SLAM_Recovery_Focused_Care_Plan_ID" == ^recovery_care_plan."SLAM_Recovery_Focused_Care_Plan_ID"
-    life_event_triggers = ReadOnlyRepo.one(life_event_triggers_query)
+      life_event_triggers = ReadOnlyRepo.one(life_event_triggers_query)
 
-    help_from_others_query = from esn in EPJSRcpHelpFromOthers,
+      help_from_others_query = from esn in EPJSRcpHelpFromOthers,
       where: esn."SLAM_Recovery_Focused_Care_Plan_ID" == ^recovery_care_plan."SLAM_Recovery_Focused_Care_Plan_ID"
-    help_from_others = ReadOnlyRepo.one(help_from_others_query)
+      help_from_others = ReadOnlyRepo.one(help_from_others_query)
 
-    goals_asp_query = from esn in EPJSRcpGoalsAsp,
+      goals_asp_query = from esn in EPJSRcpGoalsAsp,
       where: esn."SLAM_Recovery_Focused_Care_Plan_ID" == ^recovery_care_plan."SLAM_Recovery_Focused_Care_Plan_ID"
-    goals_asp = ReadOnlyRepo.one(goals_asp_query)
+      goals_asp = ReadOnlyRepo.one(goals_asp_query)
 
-    daily_activity_query = from esn in EPJSRcpDailyActivity,
+      daily_activity_query = from esn in EPJSRcpDailyActivity,
       where: esn."SLAM_Recovery_Focused_Care_Plan_ID" == ^recovery_care_plan."SLAM_Recovery_Focused_Care_Plan_ID"
-    daily_activity = ReadOnlyRepo.one(daily_activity_query)
+      daily_activity = ReadOnlyRepo.one(daily_activity_query)
 
-    contingency_query = from esn in EPJSRcpContingency,
+      contingency_query = from esn in EPJSRcpContingency,
       where: esn."SLAM_Recovery_Focused_Care_Plan_ID" == ^recovery_care_plan."SLAM_Recovery_Focused_Care_Plan_ID"
-    contingency = ReadOnlyRepo.one(contingency_query)
+      contingency = ReadOnlyRepo.one(contingency_query)
+    else
+      life_event_triggers = nil
+      help_from_others = nil
+      goals_asp = nil
+      daily_activity = nil
+      contingency = nil
+    end
+
 
     %{summary_needs: summary_needs, recovery_care_plan: recovery_care_plan,
     life_event_triggers: life_event_triggers, help_from_others: help_from_others,
