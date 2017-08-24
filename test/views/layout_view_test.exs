15af329b2922bff50fba264e5e32705ee9028e46
katbow <katherine.bowler@gmail.com>
2017-08-24 19:06:00 +0100

    Test each case in the care team function #762 #1010

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,16 @@
 defmodule Healthlocker.LayoutViewTest do
   use Healthlocker.ConnCase, async: true
+  alias Healthlocker.{LayoutView, User}
+
+  test "care_team? with caring for" do
+    assert LayoutView.care_team?(struct(User, %{caring: [1]}))
+  end
+
+  test "care_team? with no caring for and role is carer" do
+    assert LayoutView.care_team?(struct(User, %{caring: [], role: "carer"}))
+  end
+
+  test "care_team? with no caring for and role is not carer" do
+    refute LayoutView.care_team?(struct(User, %{caring: [], role: "something_else"}))
+  end
 end
