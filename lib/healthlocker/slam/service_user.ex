1a0c180ed9eec9c911b1d940f66b6e7b18b59512
katbow <katherine.bowler@gmail.com>
2017-08-25 09:44:35 +0100

    Update ServiceUser.for function to get service user when they are not in HL #762 #1011

--- lib/healthlocker/slam/service_user.ex
+++ lib/healthlocker/slam/service_user.ex
@@ -1,17 +1,21 @@
 defmodule Healthlocker.Slam.ServiceUser do
-  alias Healthlocker.Repo
-
+  alias Healthlocker.{Repo, ReadOnlyRepo, Carer, EPJSUser}
+  alias Healthlocker.ReadOnlyRepo
   @moduledoc """
   If the User has a slam_id then they're already a service user. However, if the
   user is a carer, then their service user is the user they care for.
   """
-  def for(carer) do
-    if carer.slam_id do
-      carer
-    else
-      carer = carer |> Repo.preload(:caring)
-      [service_user | _] = carer.caring
-      service_user
+  def for(user) do
+    carer = user |> Repo.preload(:caring)
+    cond do
+      user.slam_id ->
+        user
+      carer.caring == [] ->
+        carer_connection = Repo.get_by(Carer, carer_id: user.id)
+        ReadOnlyRepo.get_by(EPJSUser, Patient_ID: carer_connection.slam_id)
+      true ->
+        [service_user | _] = carer.caring
+        service_user
     end
   end
 end
