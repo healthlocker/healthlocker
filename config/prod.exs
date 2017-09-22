edeb2bae59bc73d846526d702cd815afe1cb42f8
katbow <katherine.bowler@gmail.com>
2017-09-22 11:41:43 +0100

    Increase idle_timeout to 10 s #1095

--- config/prod.exs
+++ config/prod.exs
@@ -38,7 +38,8 @@ config :healthlocker, Healthlocker.ReadOnlyRepo,
   hostname: System.get_env("READ_ONLY_HOSTNAME"),
   username: System.get_env("READ_ONLY_USERNAME"),
   password: System.get_env("READ_ONLY_PASSWORD"),
-  database: System.get_env("READ_ONLY_DATABASE")
+  database: System.get_env("READ_ONLY_DATABASE"),
+  idle_timeout: 10_000
 
 # Do not print debug messages in production
 config :logger, level: :info, format: "[$level] $message\n"
