82a90573762411a33227ff81868d5be9f534484b
katbow <katherine.bowler@gmail.com>
2017-11-29 16:27:02 +0000

    Fix test for feedback controller #1115

--- test/controllers/feedback_controller_test.exs
+++ test/controllers/feedback_controller_test.exs
@@ -11,7 +11,7 @@ defmodule Healthlocker.FeedbackControllerTest do
   test "/feedback :: create", %{conn: conn} do
     with_mock Healthlocker.Mailer, [deliver_now: fn(_) -> nil end] do
       conn = post conn, feedback_path(conn, :create,
-      %{"feedback" => %{"subject" => "Here's a subject", "content" => "With some content"}})
+      %{"feedback" => %{"subject" => "Here's a subject", "content" => "With some content", "email" => "email@example.com"}})
       assert redirected_to(conn, 302) =~ "/feedback"
     end
   end
