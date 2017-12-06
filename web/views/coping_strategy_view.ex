9020afe6392adf9187925dc8bcdbe2ad2e5a3b7e
robstallion <robertefrancis18@gmail.com>
2017-12-06 14:48:02 +0000

    update format_output so that it will work in more cases #1123

--- web/views/coping_strategy_view.ex
+++ web/views/coping_strategy_view.ex
@@ -2,9 +2,9 @@ defmodule Healthlocker.CopingStrategyView do
   use Healthlocker.Web, :view
   import Healthlocker.PostView, only: [markdown: 1]
 
-  def format_output(text) do
+  def format_output(text, tag) do
     text
-    |> String.trim_trailing(" #CopingStrategy")
+    |> String.trim_trailing(tag)
     |> markdown()
   end
 end
